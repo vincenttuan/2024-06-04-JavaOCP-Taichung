c91b2ec80474067e296fd6b8995344094c44eea8
FH <FH@DESKTOP-48IJ5S0>
2024-06-04 19:24:08 +0800

    java

---
+++ JavaOCP/src/day01/MethodDemo1.java
@@ -0,0 +1,15 @@
+package day01;
+
+// 方法的調用
+public class MethodDemo1 {
+
+	public static void main(String[] args) {
+		printWords();
+		printWords();
+	}
+	
+	public static void printWords() {
+		System.out.println("she sell sea shell on the sea shore");
+	} 
+
+}
