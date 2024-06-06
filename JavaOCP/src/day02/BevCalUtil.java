8fdc3c9435c0159c7e258c4265ad28631b7a799c
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 19:14:05 +0800

    java

---
+++ JavaOCP/src/day02/BevCalUtil.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class BevCalUtil {
+	// 定義每一種飲料每100ml(千卡)
+	public static double coffeeCalPer100ml = 2.0;
+	public static double teaCalPer100ml = 2.0;
+	public static double sodaCalPer100ml = 2.0;
+	public static double juiceCalPer100ml = 2.0;
+	
+	// 定義熱量文字
+	public static String lowCal = "低熱量";
+	public static String medCal = "中熱量";
+	public static String highCal = "高熱量";
+	
+	
+	
+}
