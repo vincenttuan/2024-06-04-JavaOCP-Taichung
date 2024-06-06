5831617e50138c71e254ec340797e67f9070ffe0
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 19:40:05 +0800

    java

--- JavaOCP/src/day02/BevCalMain.java
+++ JavaOCP/src/day02/BevCalMain.java
@@ -4,9 +4,11 @@ public class BevCalMain {
 
 	public static void main(String[] args) {
 		
-		double cal = BevCalUtil.getCal("juice", 480/100.0);
+		double cal = BevCalUtil.getCal("tea", 480/100.0);
 		System.out.println(cal);
 		
+		String calLevel = BevCalUtil.getCalLevel(cal);
+		System.out.println(calLevel);
 	}
 
 }
