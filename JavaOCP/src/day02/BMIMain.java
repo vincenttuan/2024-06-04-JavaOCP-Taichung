0a3dda9cc39d927fe4bbda8bcdcefd9996fe8b8f
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 18:52:27 +0800

    java

--- JavaOCP/src/day02/BMIMain.java
+++ JavaOCP/src/day02/BMIMain.java
@@ -1,18 +1,22 @@
 package day02;
 
+import static day02.BMIUtil.getBMI;
+import static day02.BMIUtil.getResult;
+import static day02.BMIUtil.printBMI;
+
 public class BMIMain {
 
 	public static void main(String[] args) {
 		double h1 = 170, w1 = 60;
 		double h2 = 165.5, w2 = 48.6;
 		
-		double bmi1 = BMIUtil.getBMI(h1, w1);
+		double bmi1 = getBMI(h1, w1);
 		System.out.println(bmi1);
 		
-		String result1 = BMIUtil.getResult(bmi1);
+		String result1 = getResult(bmi1);
 		System.out.println(result1);
 		
-		BMIUtil.printBMI(h1, w1, bmi1, result1);
+		printBMI(h1, w1, bmi1, result1);
 	}
 
 }
