9d4cf7a88bd20f2e549d47bf3e1c3565d3316b91
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 20:12:37 +0800

    java

--- JavaOCP/src/day02/BevCalMain.java
+++ JavaOCP/src/day02/BevCalMain.java
@@ -3,11 +3,17 @@ package day02;
 public class BevCalMain {
 
 	public static void main(String[] args) {
-		double cal = BevCalUtil.getCal("coffee", 480/100.0);
+		String bevType = "coffee";
+		double ml = 480;
+		
+		double cal = BevCalUtil.getCal(bevType, ml/100.0);
 		System.out.println(cal);
 		
 		String calLevel = BevCalUtil.getCalLevel(cal);
 		System.out.println(calLevel);
+		
+		BevCalUtil.printCal(bevType, ml, cal, calLevel);
+		
 	}
 
 }
