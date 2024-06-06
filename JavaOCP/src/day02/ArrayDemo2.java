56dad824f0fff855babd251be14179548dd82087
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 20:59:59 +0800

    java

--- JavaOCP/src/day02/ArrayDemo2.java
+++ JavaOCP/src/day02/ArrayDemo2.java
@@ -12,16 +12,29 @@ public class ArrayDemo2 {
 		System.out.println(scores[3]);
 		System.out.println(scores[4]);
 		System.out.println("-----"); // 分隔線
+		
 		// 寫一個 for-loop 印出每一個陣列元素
 		for(int i=0;i<=4;i++) { // i 用來表示維度
 			System.out.println(scores[i]);
 		}
 		System.out.println("-----"); // 分隔線
+		
 		for(int i=0;i<scores.length;i++) { // i 用來表示維度
 			System.out.println(scores[i]);
 		}
 		System.out.println("-----"); // 分隔線
 		
+		for(int i=0, lens=scores.length;i<lens;i++) { // i 用來表示維度
+			System.out.println(scores[i]);
+		}
+		System.out.println("-----"); // 分隔線
+		
+		// 寫一個 for-each 印出每一個陣列元素
+		// for-each 屬於高階迴圈會自動尋訪下一個元素 
+		for(int score : scores) {
+			System.out.println(score);
+		}
+		System.out.println("-----"); // 分隔線
 	}
 
 }
