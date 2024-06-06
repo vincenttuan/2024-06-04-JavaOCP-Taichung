fbcefc52998bc7e69921d0a4b1883c4aef2030cd
FH <FH@DESKTOP-48IJ5S0>
2024-06-06 19:34:41 +0800

    java

---
+++ JavaOCP/src/day02/BevCalMain.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class BevCalMain {
+
+	public static void main(String[] args) {
+		
+		double cal = BevCalUtil.getCal("juice", 480/100.0);
+		System.out.println(cal);
+		
+	}
+
+}
