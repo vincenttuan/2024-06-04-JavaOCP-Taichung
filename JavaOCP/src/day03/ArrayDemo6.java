254fc222d971bdf1dd1228cfde87026407e3a0af
FH <FH@DESKTOP-48IJ5S0>
2024-06-11 20:48:04 +0800

    java

---
+++ JavaOCP/src/day03/ArrayDemo6.java
@@ -0,0 +1,21 @@
+package day03;
+
+public class ArrayDemo6 {
+
+	public static void main(String[] args) {
+		int[] scoreA = {80, 85, 100, 50, 70, 30};
+		int[] scoreB = {45, 60, 55, 65, 62, 58};
+		
+		// 試問 A, B 二班哪一班成績較為集中
+		double avgA = IntArrayUtil.getAvg(scoreA);
+		double sumA = 0;
+		for(int i=0, lens=scoreA.length ; i<lens ; i++) {
+			sumA += Math.pow(scoreA[i] - avgA, 2);
+		}
+		// A 班標準差
+		double sdA = Math.sqrt(sumA / scoreA.length);
+		System.out.printf("A 班標準差: %.1f%n", sdA);
+
+	}
+
+}
