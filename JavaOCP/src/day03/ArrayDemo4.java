96f1ba16ff479127764217cc36dcdb0a7e409ed5
FH <FH@DESKTOP-48IJ5S0>
2024-06-11 19:07:33 +0800

    java

--- JavaOCP/src/day03/ArrayDemo4.java
+++ JavaOCP/src/day03/ArrayDemo4.java
@@ -6,8 +6,14 @@ public class ArrayDemo4 {
 		// 分數陣列
 		int[] scores = {70, 80, 100, 90, 40, 60};
 		// 找最高分
+		int highScore = scores[0]; // 假設最高分是 scores[0]
 		
-		
+		for(int i=1, lens=scores.length ; i<lens ; i++) {
+			if(scores[i] > highScore) {
+				highScore = scores[i]; // 替換最高分
+			}
+		}
+		System.out.printf("最高分: %d%n", highScore);
 
 	}
 
