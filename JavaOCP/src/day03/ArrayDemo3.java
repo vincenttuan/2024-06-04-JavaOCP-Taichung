80d5f1a35c5876ff16ee5d0cffb703855f09f530
FH <FH@DESKTOP-48IJ5S0>
2024-06-11 18:50:19 +0800

    java

--- JavaOCP/src/day03/ArrayDemo3.java
+++ JavaOCP/src/day03/ArrayDemo3.java
@@ -7,14 +7,16 @@ public class ArrayDemo3 {
 	public static void main(String[] args) {
 		int[] scores = new int[5]; // 預設內容 => {0, 0, 0, 0, 0}
 		// 印出陣列元素
-		System.out.println(Arrays.toString(scores));
+		System.out.println("scores:" + Arrays.toString(scores));
 		// 指定元素內容
 		scores[1] = 99;
 		scores[2] = 77;
 		scores[3] = 88;
 		// 印出陣列元素
-		System.out.println(Arrays.toString(scores));
-		
+		System.out.println("scores:" + Arrays.toString(scores));
+		// 陣列複製
+		int[] newScores = new int[3]; // 預設內容 => {0, 0, 0}
+		System.out.println("newScores:" + Arrays.toString(newScores));
 	}
 
 }
