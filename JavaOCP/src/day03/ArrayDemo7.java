449c176ef9e2c5134f4546085d79af3f56cf9237
FH <FH@DESKTOP-48IJ5S0>
2024-06-11 21:09:21 +0800

    java

---
+++ JavaOCP/src/day03/ArrayDemo7.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class ArrayDemo7 {
+	public static void main(String[] args) {
+		int[] scores = {80, 85, 100, 50, 70, 30}; // 成績
+		int[] heights = {170, 180, 168, 150, 143, 166}; // 身高
+		
+		// 成績與身高哪一個集中 ?
+		// 變異係數: 標準差/平均
+		// CV = SD / AVG
+		
+		
+		
+	}
+}
