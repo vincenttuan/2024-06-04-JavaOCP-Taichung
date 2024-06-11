0e32d35f31799ba0a179f494abb95829e537b62a
FH <FH@DESKTOP-48IJ5S0>
2024-06-11 18:45:03 +0800

    java

---
+++ JavaOCP/src/day03/ArrayDemo3.java
@@ -0,0 +1,19 @@
+package day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo3 {
+
+	public static void main(String[] args) {
+		int[] scores = new int[5]; // 預設內容 => {0, 0, 0, 0, 0}
+		// 印出陣列元素
+		System.out.println(Arrays.toString(scores));
+		// 指定元素內容
+		scores[1] = 99;
+		scores[3] = 88;
+		// 印出陣列元素
+		System.out.println(Arrays.toString(scores));
+
+	}
+
+}
