1b2d6230ab50e80478126d33602dc188b683908b
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 19:37:15 +0800

    java

--- JavaOCP/src/day04/SimpleBubbleSort2.java
+++ JavaOCP/src/day04/SimpleBubbleSort2.java
@@ -9,11 +9,23 @@ public class SimpleBubbleSort2 {
 		int[] scores = {10, 8, 6, 7, 5};
 		// 排序前
 		System.out.println("排序前:" + Arrays.toString(scores));
-		
+		int n = scores.length;
+		//---------------------------------------------------------
+		for(int j=0;j<n-1;j++) {
+			for(int i=0;i<n-1-j;i++) {
+				if(scores[i] > scores[i+1]) {
+					// 資料互換
+					int tmp = scores[i+1];
+					scores[i+1] = scores[i];
+					scores[i] = tmp;
+				}
+			}
+		}
+		//---------------------------------------------------------
 		
 		// 排序後
 		System.out.println("排序後:" + Arrays.toString(scores));
-
+		
 	}
 
 }
