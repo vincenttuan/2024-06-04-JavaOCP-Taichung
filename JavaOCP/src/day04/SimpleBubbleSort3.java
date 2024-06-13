a1582a072bb1d6309596c9d69517b829bcef6e67
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 19:46:51 +0800

    java

--- JavaOCP/src/day04/SimpleBubbleSort3.java
+++ JavaOCP/src/day04/SimpleBubbleSort3.java
@@ -5,13 +5,17 @@ import java.util.Arrays;
 public class SimpleBubbleSort3 {
 
 	public static void main(String[] args) {
-		// 泡沫排序法(大到小)
 		int[] scores = {10, 8, 6, 7, 5};
 		// 排序前
 		System.out.println("排序前:" + Arrays.toString(scores));
 		
 		// 排序後
-		System.out.println("排序後:" + Arrays.toString(scores));
+		bubbleSort(scores, true);
+		System.out.println("泡沫排序法(小到大):" + Arrays.toString(scores));
+		
+		bubbleSort(scores, false);
+		System.out.println("泡沫排序法(大到小):" + Arrays.toString(scores));
+		
 		
 	}
 	
