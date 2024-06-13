729dab8f9211efdd56362a3ad41b96f95a544054
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 18:38:43 +0800

    java

--- JavaOCP/src/day04/PrimeCheck.java
+++ JavaOCP/src/day04/PrimeCheck.java
@@ -7,12 +7,22 @@ public class PrimeCheck {
 		// int[] values = {10, 75, 13, 143, 51};
 		
 		// 除了 1 與自己之外, x 有哪些因數
-		int x = 74;
+		int x = 51;
 		for(int i=2 ; i<=x/2 ; i++) {
 			if(x % i == 0) {
 				System.out.printf("%d 是 %d 的因數%n", i, x);
 			}
 		}
+		
+		// x 是否是質數
+		boolean isPrime = true; // 假設是質數
+		for(int i=2 ; i<=x/2 ; i++) {
+			if(x % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		System.out.printf("%d 是質數? %b%n", x, isPrime);
 		
 	}
 
