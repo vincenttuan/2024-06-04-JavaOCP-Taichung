808be39c7cd07ed9bc46c726444bec79861985d0
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 18:40:36 +0800

    java

--- JavaOCP/src/day04/FindAllPrimeNumber.java
+++ JavaOCP/src/day04/FindAllPrimeNumber.java
@@ -4,8 +4,8 @@ public class FindAllPrimeNumber {
 
 	public static void main(String[] args) {
 		// 找出質數並印出
-		// int[] values = {10, 75, 13, 107, 143, 51};
-
+		int[] values = {10, 75, 13, 107, 143, 51};
+		
 	}
 
 }
