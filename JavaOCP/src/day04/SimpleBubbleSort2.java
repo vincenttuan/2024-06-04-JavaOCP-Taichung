f35b832050549e75b2c9081fb04b667bd0a626f3
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 19:16:24 +0800

    java

---
+++ JavaOCP/src/day04/SimpleBubbleSort2.java
@@ -0,0 +1,19 @@
+package day04;
+
+import java.util.Arrays;
+
+public class SimpleBubbleSort2 {
+
+	public static void main(String[] args) {
+		// 泡沫排序法
+		int[] scores = {10, 8, 6, 7, 5};
+		// 排序前
+		System.out.println("排序前:" + Arrays.toString(scores));
+		
+		
+		// 排序後
+		System.out.println("排序後:" + Arrays.toString(scores));
+
+	}
+
+}
