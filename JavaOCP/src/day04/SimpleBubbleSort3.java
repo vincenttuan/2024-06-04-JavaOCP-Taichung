0251d3c4afbd94c4f3c94f021f278b2b7cf31004
FH <FH@DESKTOP-48IJ5S0>
2024-06-13 19:38:00 +0800

    java

---
+++ JavaOCP/src/day04/SimpleBubbleSort3.java
@@ -0,0 +1,18 @@
+package day04;
+
+import java.util.Arrays;
+
+public class SimpleBubbleSort3 {
+
+	public static void main(String[] args) {
+		// 泡沫排序法(大到小)
+		int[] scores = {10, 8, 6, 7, 5};
+		// 排序前
+		System.out.println("排序前:" + Arrays.toString(scores));
+		
+		// 排序後
+		System.out.println("排序後:" + Arrays.toString(scores));
+		
+	}
+
+}
