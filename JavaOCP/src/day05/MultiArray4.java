9490db5e777ce03f8d62452e7f8537e3fad14641
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 19:31:08 +0800

    java

---
+++ JavaOCP/src/day05/MultiArray4.java
@@ -0,0 +1,13 @@
+package day05;
+
+public class MultiArray4 {
+	public static void main(String[] args) {
+		// 建立一個 2x3 的二維陣列-含有初始值
+		int[][] x = {
+						{90, 80, 100}, 
+						{70, 50, 60}
+					};
+		// 利用 java stream 取最大值
+		
+	}
+}
