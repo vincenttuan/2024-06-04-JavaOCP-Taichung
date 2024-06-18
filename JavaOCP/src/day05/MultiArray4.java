4b60360b1c9d9f96118858a1bf7db48c7d9509fd
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 19:39:11 +0800

    java

--- JavaOCP/src/day05/MultiArray4.java
+++ JavaOCP/src/day05/MultiArray4.java
@@ -1,5 +1,8 @@
 package day05;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class MultiArray4 {
 	public static void main(String[] args) {
 		// 建立一個 2x3 的二維陣列-含有初始值
@@ -7,7 +10,19 @@ public class MultiArray4 {
 						{90, 80, 100}, 
 						{70, 50, 60}
 					};
-		// 利用 java stream 取最大值
+		/*
+			利用 java stream 取最大值
+		  	將二維陣列轉一維陣列
+		  	{
+			  {90, 80, 100},     =>   {90, 80, 100, 70, 50, 60}
+			  {70, 50, 60}    flatMap
+			};
+		*/
+		int max = Arrays.stream(x) // 將 x 二維陣列轉串流
+					 	.flatMapToInt(array -> IntStream.of(array)) // 一維陣列轉int串流
+					 	.max()
+					 	.getAsInt();
 		
+		System.out.printf("max 最大值 = %d%n", max);
 	}
 }
