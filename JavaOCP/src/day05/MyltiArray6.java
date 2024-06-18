4ad49b0bfdec8ecf1d91b434672cdd43e127a4d3
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 20:54:40 +0800

    java

--- JavaOCP/src/day05/MyltiArray6.java
+++ JavaOCP/src/day05/MyltiArray6.java
@@ -8,6 +8,25 @@ public class MyltiArray6 {
 						{50, -1, -70, 90}
 					};
 		// 取得 score >= 0 的總分
+		int sum1 = 0;
+		for(int i=0 ; i<x.length ; i++) {
+			for(int k=0 ; k<x[i].length ; k++) {
+				if(x[i][k] >= 0) {
+					sum1 += x[i][k];
+				}
+			}
+		}
+		System.out.printf("總分: %d%n", sum1);
 		
+		int sum2 = 0;
+		for(int[] array : x) {
+			for(int value : array) {
+				if(value < 0) {
+					continue;
+				}
+				sum2 += value;
+			}
+		}
+		System.out.printf("總分: %d%n", sum2);
 	}
 }
