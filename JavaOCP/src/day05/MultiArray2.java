fa06ac3a7089b10eaaf303b378c7de0dd4b0947d
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 18:58:09 +0800

    java

--- JavaOCP/src/day05/MultiArray2.java
+++ JavaOCP/src/day05/MultiArray2.java
@@ -6,8 +6,8 @@ public class MultiArray2 {
 	public static void main(String[] args) {
 		// 建立一個 2x3 的二維陣列-含有初始值
 		int[][] x = {
-						{100, 90, 80}, 
-						{70, 60, 50}
+						{90, 80, 100}, 
+						{70, 50, 60}
 					};
 		System.out.println(x.length); // 2
 		//System.out.println(Arrays.toString(x[0]));
