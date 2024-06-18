d98a5624b384d9646c76c5eb1502e43c3685fc34
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 18:53:39 +0800

    java

--- JavaOCP/src/day05/MultiArray1.java
+++ JavaOCP/src/day05/MultiArray1.java
@@ -8,6 +8,14 @@ public class MultiArray1 {
 		// 建立一個 3x3 的二維陣列
 		char[][] x = new char[3][3];
 		x[0][0] = 'O';
+		x[0][1] = 'X';
+		x[0][2] = 'O';
+		x[1][0] = 'O';
+		x[1][1] = 'O';
+		x[1][2] = 'X';
+		x[2][0] = 'X';
+		x[2][1] = 'X';
+		x[2][2] = 'O';
 		System.out.println(Arrays.toString(x[0]));
 		System.out.println(Arrays.toString(x[1]));
 		System.out.println(Arrays.toString(x[2]));
