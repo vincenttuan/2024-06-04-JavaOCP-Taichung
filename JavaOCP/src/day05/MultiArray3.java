fae188f97b45310bd2695119f5156df9482dd970
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 19:01:38 +0800

    java

---
+++ JavaOCP/src/day05/MultiArray3.java
@@ -0,0 +1,13 @@
+package day05;
+
+public class MultiArray3 {
+	public static void main(String[] args) {
+		// 建立一個 2x3 的二維陣列-含有初始值
+		int[][] x = {
+						{90, 80, 100}, 
+						{70, 50, 60}
+					};
+		// 請找出最大值 = ?
+		
+	}
+}
