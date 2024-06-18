aeaccc3899733da45e83d964a8f55b82e1c4247b
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 18:32:23 +0800

    java

---
+++ JavaOCP/src/day05/MultiArray1.java
@@ -0,0 +1,18 @@
+package day05;
+
+import java.util.Arrays;
+
+public class MultiArray1 {
+
+	public static void main(String[] args) {
+		// 建立一個 3x3 的二維陣列
+		char[][] x = new char[3][3];
+		x[0][0] = 'O';
+		System.out.println(Arrays.toString(x[0]));
+		System.out.println(Arrays.toString(x[1]));
+		System.out.println(Arrays.toString(x[2]));
+		
+
+	}
+
+}
