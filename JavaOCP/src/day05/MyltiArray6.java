026cefe3741884ab738e37fd54a392b22c9d78ff
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 20:43:46 +0800

    java

---
+++ JavaOCP/src/day05/MyltiArray6.java
@@ -0,0 +1,13 @@
+package day05;
+
+public class MyltiArray6 {
+	public static void main(String[] args) {
+		int[][] x = {
+						{100, -10, 70},
+						{80, 60},
+						{50, -1, -70, 90}
+					};
+		// 取得 score >= 0 的總分
+		
+	}
+}
