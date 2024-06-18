13e9d9b7f2b27ef09db6e9fc3e4ee753b27afc38
FH <FH@DESKTOP-48IJ5S0>
2024-06-18 19:08:45 +0800

    java

--- JavaOCP/src/day05/MultiArray3.java
+++ JavaOCP/src/day05/MultiArray3.java
@@ -7,12 +7,14 @@ public class MultiArray3 {
 						{90, 80, 100}, 
 						{70, 50, 60}
 					};
-		// 請找出最大值 = ?
+		
+		// 印出每一個元素
 		for(int i=0 ; i<x.length ; i++) {
 			// x[i] 是一維陣列
 			for(int k=0 ; k < x[i].length ; k++) {
 				System.out.printf("[%d][%d]=%d%n", i, k, x[i][k]);
 			}
 		}
+		// 請找出最大值 = ?
 	}
 }
