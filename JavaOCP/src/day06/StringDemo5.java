dd8d137e6dbcd969dd8585d76ef33e6b276df34b
FH <FH@DESKTOP-48IJ5S0>
2024-06-20 21:07:04 +0800

    java

--- JavaOCP/src/day06/StringDemo5.java
+++ JavaOCP/src/day06/StringDemo5.java
@@ -14,6 +14,29 @@ public class StringDemo5 {
 		System.out.printf("第一個字:%s%n", wordArray[0]);
 		System.out.printf("最後一個字:%s%n", wordArray[wordArray.length-1]);
 		System.out.printf("總共有:%d%n", wordArray.length);
+		
+		String s1 = null; // 未初始
+		String s2 = "";   // 有初始, 空字串資料
+		System.out.println(s1);
+		//System.out.println(s1.length()); // 沒初始的字串是無法對該字串變數進行任何操作
+		System.out.println(s2);
+		System.out.println(s2.length());
+		
+		String s3 = "";
+		String s4 = "  ";
+		System.out.println(s3.isBlank());
+		System.out.println(s3.isEmpty());
+		System.out.println(s4.isBlank());
+		System.out.println(s4.isEmpty());
+		
+		String s5 = "  ab cd  ";
+		System.out.println(s5);
+		System.out.println(s5.length());
+		s5 = s5.trim(); // 去除左右空白
+		System.out.println(s5);
+		System.out.println(s5.length());
+		
+		
 	}
 
 }
