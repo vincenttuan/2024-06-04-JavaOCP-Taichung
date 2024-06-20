acecb87161d1e7389bf0db6086dc3ca956092f51
FH <FH@DESKTOP-48IJ5S0>
2024-06-20 20:27:41 +0800

    java

---
+++ JavaOCP/src/day06/StringDemo4.java
@@ -0,0 +1,11 @@
+package day06;
+
+public class StringDemo4 {
+
+	public static void main(String[] args) {
+		String words = "she sell sea shell on the sea shore";
+		System.out.printf("英文繞口令: %s%n", words);
+
+	}
+
+}
