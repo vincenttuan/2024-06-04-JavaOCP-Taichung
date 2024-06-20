8436b7e21077faf8fe767d8764d2314be82bc9b1
FH <FH@DESKTOP-48IJ5S0>
2024-06-20 20:38:06 +0800

    java

--- JavaOCP/src/day06/StringDemo4.java
+++ JavaOCP/src/day06/StringDemo4.java
@@ -4,8 +4,17 @@ public class StringDemo4 {
 
 	public static void main(String[] args) {
 		String words = "she sell sea shell on the sea shore";
+		//              0123456789...
 		System.out.printf("英文繞口令: %s%n", words);
-
+		// words 是否有 sea 這個字串 ?
+		System.out.println(words.contains("sea"));
+		System.out.println(words.contains("water"));
+		System.out.println(words.indexOf("sea"));
+		System.out.println(words.indexOf("water"));
+		// words 字串長度
+		System.out.println(words.length());
+		
+		
 	}
 
 }
