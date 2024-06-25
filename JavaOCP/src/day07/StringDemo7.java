84f4e52082793f66489461910087e357e091cdab
FH <FH@DESKTOP-48IJ5S0>
2024-06-25 18:41:12 +0800

    java

---
+++ JavaOCP/src/day07/StringDemo7.java
@@ -0,0 +1,13 @@
+package day07;
+
+public class StringDemo7 {
+
+	public static void main(String[] args) {
+		// StringBuffer 可變動字串
+		StringBuffer sb = new StringBuffer("Java");
+		sb.append("OCP");
+		System.out.println(sb);
+		//System.out.println(sb.toString());
+	}
+
+}
