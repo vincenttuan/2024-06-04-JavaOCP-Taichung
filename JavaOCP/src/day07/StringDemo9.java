9bad9f285c314edef5ca8e9f1525a0c3cbe907d3
FH <FH@DESKTOP-48IJ5S0>
2024-06-25 19:14:24 +0800

    java

--- JavaOCP/src/day07/StringDemo9.java
+++ JavaOCP/src/day07/StringDemo9.java
@@ -6,10 +6,19 @@ public class StringDemo9 {
 		s = s.concat("8");
 		return s;
 	}
+	
+	public static int update(int foo) {
+		foo = foo + 1;
+		return foo;
+	}
 		
 	public static void main(String[] args) {
 		String a = "Java";
 		a = update(a);
 		System.out.println(a);
+		//--------------------------------
+		int bar = 10;
+		bar = update(bar);
+		System.out.println(bar);
 	}
 }
