afe6227ebc32e6c0dc50cb56c40b0ede3feb1320
FH <FH@DESKTOP-48IJ5S0>
2024-06-25 20:31:26 +0800

    java

--- JavaOCP/src/day07/WrapperDemo3.java
+++ JavaOCP/src/day07/WrapperDemo3.java
@@ -7,10 +7,13 @@ public class WrapperDemo3 {
 	public static void main(String[] args) {
 		// int vs Integer ?
 		int[] scoresA = new int[5];
+		scoresA[1] = 0;
 		Integer[] scoresB = new Integer[5];
+		scoresB[1] = 0;
 		
 		System.out.println(Arrays.toString(scoresA));
 		System.out.println(Arrays.toString(scoresB));
+		
 	}
 
 }
