6d55110f4ed4643ab501c6bbad1f0899fb7b9b58
FH <FH@DESKTOP-48IJ5S0>
2024-06-25 20:45:25 +0800

    java

--- JavaOCP/src/day07/WrapperDemo4.java
+++ JavaOCP/src/day07/WrapperDemo4.java
@@ -1,10 +1,12 @@
 package day07;
 
+import java.util.Arrays;
+
 public class WrapperDemo4 {
 
 	public static void main(String[] args) {
 		Integer[] scores = {100, null, 90, null, 80}; // Auto-Boxing
-		// 計算總分
+		// 計算總分: for-each
 		int sum = 0;
 		for(Integer score : scores) { 
 			if(score == null) {
@@ -13,6 +15,13 @@ public class WrapperDemo4 {
 			sum += score; // Auto-Unboxing
 		}
 		System.out.println(sum);
+		
+		// 計算總分: Stream
+		int sum2 =  Arrays.stream(scores) // Integer 串流
+						  .filter(score -> score != null) // Integer 串流
+						  .mapToInt(score -> score.intValue()) // int 串流: IntStream
+						  .sum();
+		System.out.println(sum2);
 	}
 
 }
