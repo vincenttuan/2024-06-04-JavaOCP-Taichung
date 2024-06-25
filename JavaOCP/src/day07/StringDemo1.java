609c499fd1f6013531cc7ba58a2da802e41b1439
FH <FH@DESKTOP-48IJ5S0>
2024-06-25 18:19:49 +0800

    java

---
+++ JavaOCP/src/day07/StringDemo1.java
@@ -0,0 +1,13 @@
+package day07;
+
+public class StringDemo1 {
+	public static void main(String[] args) {
+		// 字串 immutable
+		String s = "Java";
+		System.out.println(s);
+		s = s + "OCP";
+		System.out.println(s);
+		
+		
+	}
+}
