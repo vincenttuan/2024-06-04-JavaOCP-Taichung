4a75d824ec34dddbc5ab48186b2cbd07278598ac
FH <FH@DESKTOP-48IJ5S0>
2024-06-27 20:41:27 +0800

    java

---
+++ JavaOCP/src/day08/oo/Meal.java
@@ -0,0 +1,35 @@
+package day08.oo;
+
+// 組合餐
+public class Meal {
+	private String name;
+	private Integer price;
+	private String hamburgerName; // 主餐名(漢堡)
+	private String beverageName;  // 飲料名
+	private String sideMealName;  // 副餐名
+	
+	// 建構子
+	public Meal(String name, Integer price, String hamburgerName, String beverageName, String sideMealName) {
+		this.name = name;
+		this.price = price;
+		this.hamburgerName = hamburgerName;
+		this.beverageName = beverageName;
+		this.sideMealName = sideMealName;
+	}
+
+	public void setBeverageName(String beverageName) {
+		this.beverageName = beverageName;
+	}
+
+	public void setSideMealName(String sideMealName) {
+		this.sideMealName = sideMealName;
+	}
+	
+	public void displayMealInfo() {
+		System.out.printf("%s餐 價格: %d 漢堡: %s 飲料: %s 副餐: %s%n", 
+				name, price, hamburgerName, beverageName, sideMealName);
+	}
+	
+	
+	
+}
