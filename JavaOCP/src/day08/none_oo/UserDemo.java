2f61549a45f144ba1951490050918b7d05528e50
FH <FH@DESKTOP-48IJ5S0>
2024-06-27 18:52:36 +0800

    java

--- JavaOCP/src/day08/none_oo/UserDemo.java
+++ JavaOCP/src/day08/none_oo/UserDemo.java
@@ -8,16 +8,24 @@ public class UserDemo {
 		double height1 = 175;
 		double weight1 = 68;
 		double bmi1 = weight1 / Math.pow(height1/100, 2);
+		displayUserInfo(name1, height1, weight1, bmi1);
 		
 		String name2 = "Alice";
 		double height2 = 160.0;
 		double weight2 = 55;
 		double bmi2 = weight2 / Math.pow(height2/100, 2);
+		displayUserInfo(name2, height2, weight2, bmi2);
 		
 		String name3 = "Bob";
 		double height3 = 182.5;
 		double weight3 = 85.0;
 		double bmi3 = weight3 / Math.pow(height3/100, 2);
+		displayUserInfo(name3, height3, weight3, bmi3);
+	}
+	
+	public static void displayUserInfo(String name, double height, double weight, double bmi) {
+		System.out.printf("姓名: %s 身高: %.1f 體重: %.1f BMI: %.2f%n",
+				name, height, weight, bmi);
 	}
 
 }
