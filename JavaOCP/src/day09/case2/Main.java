a9a80158b77e22f8cb75dcf68a1ff3b5effee0a9
FH <FH@DESKTOP-48IJ5S0>
2024-07-02 20:41:03 +0800

    java

--- JavaOCP/src/day09/case2/Main.java
+++ JavaOCP/src/day09/case2/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Ham ham1 = new Ham("金華", 10.0, 3);
-		Ham ham2 = new Ham("新東陽", 10.0, 2);
+		Ham ham2 = new Ham("東陽", 10.0, 2);
 		Egg egg = new Egg(2, 15);
 		Rice rice = new Rice(10.0, 1);
 		
 		System.out.println("素材");
-		System.out.printf("%s火腿 %.1f克 每克 $%d 元%n", ham1.getName(), ham1.getWeight(), ham1.getPrice());
-		System.out.printf("%s火腿 %.1f克 每克 $%d 元%n", ham2.getName(), ham2.getWeight(), ham2.getPrice());
-		System.out.printf("雞蛋 %d 顆 每顆 %d 元%n", egg.getAmount(), egg.getPrice());
-		System.out.printf("米飯 %.1f 克 每克 %d 元%n", rice.getWeight(), rice.getPrice());
-		
+		System.out.printf("%s火腿:%.1f克 每克:$%d%n", ham1.getName(), ham1.getWeight(), ham1.getPrice());
+		System.out.printf("%s火腿:%.1f克 每克:$%d%n", ham2.getName(), ham2.getWeight(), ham2.getPrice());
+		System.out.printf("雞蛋:%d 顆 每顆:$%d%n", egg.getAmount(), egg.getPrice());
+		System.out.printf("米飯:%.1f 克 每克:$%d%n", rice.getWeight(), rice.getPrice());
+		System.out.println("-----------------------");
 		// 蛋炒飯
 		FriedRice eggFriedRice = new FriedRice(egg, rice);
 		// 金華火腿蛋炒飯
