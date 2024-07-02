272b9f2f548a0e8b1b2e37855f6cec49c84d21a4
FH <FH@DESKTOP-48IJ5S0>
2024-07-02 20:47:54 +0800

    java

---
+++ JavaOCP/src/day09/case2/Main2.java
@@ -0,0 +1,15 @@
+package day09.case2;
+
+public class Main2 {
+	public static void main(String[] args) {
+		// 素材
+		Ham ham1 = new Ham("金華", 10.0, 3);
+		Ham ham2 = new Ham("東陽", 10.0, 2);
+		Egg egg = new Egg(2, 15);
+		Rice rice = new Rice(10.0, 1);
+		
+		// 點餐
+		// 3份蛋炒飯 + 2份金華火腿蛋炒飯 + 1份東陽火腿蛋炒飯 = ? 元
+		
+	}
+}
