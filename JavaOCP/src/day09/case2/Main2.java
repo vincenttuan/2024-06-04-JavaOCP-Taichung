fe7b8ec69c75e0bfc50a6c5030d7a826f67ebe24
FH <FH@DESKTOP-48IJ5S0>
2024-07-02 21:08:38 +0800

    java

--- JavaOCP/src/day09/case2/Main2.java
+++ JavaOCP/src/day09/case2/Main2.java
@@ -10,6 +10,14 @@ public class Main2 {
 		
 		// 點餐
 		// 3份蛋炒飯 + 2份金華火腿蛋炒飯 + 1份東陽火腿蛋炒飯 = ? 元
+		FriedRice f3 = new FriedRice(egg, rice);
+		FriedRice f2 = new FriedRice(ham1, egg, rice);
+		FriedRice f1 = new FriedRice(ham2, egg, rice);
+		
+		double total = f3.getPrice() * 3 +
+					   f2.getPrice() * 2 +
+					   f1.getPrice() * 1;
+		System.out.println(total);
 		
 	}
 }
