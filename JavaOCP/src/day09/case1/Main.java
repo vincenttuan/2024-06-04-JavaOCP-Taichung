30cc6c9d1d2d5247bbe946517e25499dbbbefb79
FH <FH@DESKTOP-48IJ5S0>
2024-07-02 18:43:31 +0800

    java

---
+++ JavaOCP/src/day09/case1/Main.java
@@ -0,0 +1,14 @@
+package day09.case1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FriedRice friedRice1 = new FriedRice();
+		friedRice1.setHamAmount(10.5);
+		friedRice1.setEggAmount(3);
+		friedRice1.setRiceAmount(0.5);
+		friedRice1.cook();
+
+	}
+
+}
