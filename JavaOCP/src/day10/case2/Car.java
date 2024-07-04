34f2844c959f01094723973ea3c2a19a2e95ef5e
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 19:30:59 +0800

    java

--- JavaOCP/src/day10/case2/Car.java
+++ JavaOCP/src/day10/case2/Car.java
@@ -1,5 +1,11 @@
 package day10.case2;
 
 public class Car {
-
+	// 物件變數
+	private String model; // 型號
+	private String color; // 顏色
+	private Integer maxSpeed; // 最高速度
+	private Integer price; // 價格
+	
+	
 }
--- JavaOCP/src/day10/case2/CarUtil.java
+++ JavaOCP/src/day10/case2/CarUtil.java
@@ -1,5 +1,7 @@
 package day10.case2;
 
 public class CarUtil {
-
+	// 類別變數(靜態變數) 
+	private static int totalCars; // 用於計算所有創建的汽車總數  
+	private static double discountRate; // 用於計算統一折扣
 }
