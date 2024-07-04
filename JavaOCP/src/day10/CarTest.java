c005362966739526f18e225d9d6b30e7d1ec084e
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 18:39:37 +0800

    java

---
+++ JavaOCP/src/day10/CarTest.java
@@ -0,0 +1,17 @@
+package day10;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		Car car1 = new Car("S300", "Red", 280);
+		Car car2 = new Car("A100", "Blue", 180);
+		Car car3 = new Car("T200", "Yellow", 300);
+		
+		car1.displayInfo();
+		car2.displayInfo();
+		car3.displayInfo();
+		
+
+	}
+
+}
