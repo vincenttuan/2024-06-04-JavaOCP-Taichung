47b208b0f1937deadeb01fbc025fbd4c0b2cef35
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 20:31:58 +0800

    java

--- JavaOCP/src/day10/case3/Clothing.java
+++ JavaOCP/src/day10/case3/Clothing.java
@@ -2,5 +2,13 @@ package day10.case3;
 
 public class Clothing extends Product {
 	private String size; // 尺寸
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
 	
 }
