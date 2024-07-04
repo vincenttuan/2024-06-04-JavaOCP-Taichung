6df03d62b473951a5d7bb7b5cb2de341681d7c38
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 20:31:39 +0800

    java

--- JavaOCP/src/day10/case3/Product.java
+++ JavaOCP/src/day10/case3/Product.java
@@ -3,4 +3,20 @@ package day10.case3;
 public class Product {
 	private String name; // 品名
 	private Integer price; // 價格
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+	
 }
