308b1e99196d6f35640a82321bf120b17db915a7
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 18:45:47 +0800

    java

--- JavaOCP/src/day10/Car.java
+++ JavaOCP/src/day10/Car.java
@@ -5,11 +5,14 @@ public class Car {
 	private String model; // 型號
 	private String color; // 顏色
 	private Integer maxSpeed; // 最高速度
+	// 類別變數(靜態變數) 
+	static int totalCars; // 用於計算所有創建的汽車總數  
 	
 	public Car(String model, String color, Integer maxSpeed) {
 		this.model = model;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
+		totalCars++; // 每創建一台車, 總數+1
 	}
 	
 	public void displayInfo() {
