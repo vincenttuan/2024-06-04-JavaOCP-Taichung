e08b0bb1626b441c2cbbe96307df2dfcacfe73ad
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 19:31:51 +0800

    java

--- JavaOCP/src/day10/case2/CarUtil.java
+++ JavaOCP/src/day10/case2/CarUtil.java
@@ -4,4 +4,20 @@ public class CarUtil {
 	// 類別變數(靜態變數) 
 	private static int totalCars; // 用於計算所有創建的汽車總數  
 	private static double discountRate; // 用於計算統一折扣
+	
+	public static int getTotalCars() {
+		return totalCars;
+	}
+	
+	public static void setTotalCars(int totalCars) {
+		CarUtil.totalCars = totalCars;
+	}
+	
+	public static double getDiscountRate() {
+		return discountRate;
+	}
+	
+	public static void setDiscountRate(double discountRate) {
+		CarUtil.discountRate = discountRate;
+	}
 }
