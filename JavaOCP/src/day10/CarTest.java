2afb964f422dd9de5999fecfbecf76db1ad11e15
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 19:00:57 +0800

    java

--- JavaOCP/src/day10/CarTest.java
+++ JavaOCP/src/day10/CarTest.java
@@ -3,9 +3,9 @@ package day10;
 public class CarTest {
 
 	public static void main(String[] args) {
-		Car car1 = new Car("S300", "Red", 280);
-		Car car2 = new Car("A100", "Blue", 180);
-		Car car3 = new Car("T200", "Yellow", 300);
+		Car car1 = new Car("S300", "Red", 280, 350);
+		Car car2 = new Car("A100", "Blue", 180, 250);
+		Car car3 = new Car("T200", "Yellow", 300, 450);
 		
 		car1.displayInfo();
 		car2.displayInfo();
