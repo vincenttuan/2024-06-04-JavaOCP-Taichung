70debdc9f08ab427cd3cba68f7c4dd44539cae46
FH <FH@DESKTOP-48IJ5S0>
2024-07-04 19:00:25 +0800

    java

--- JavaOCP/src/day10/Car.java
+++ JavaOCP/src/day10/Car.java
@@ -5,18 +5,22 @@ public class Car {
 	private String model; // 型號
 	private String color; // 顏色
 	private Integer maxSpeed; // 最高速度
+	private Integer price; // 價格
 	// 類別變數(靜態變數) 
 	static int totalCars; // 用於計算所有創建的汽車總數  
+	static double discountRate; // 用於計算統一折扣
 	
-	public Car(String model, String color, Integer maxSpeed) {
+	public Car(String model, String color, Integer maxSpeed, Integer price) {
 		this.model = model;
 		this.color = color;
 		this.maxSpeed = maxSpeed;
+		this.price = price;
 		totalCars++; // 每創建一台車, 總數+1
 	}
 	
 	public void displayInfo() {
-		System.out.printf("型號:%s 顏色:%s 最高速度:%d%n", model, color, maxSpeed);
+		System.out.printf("型號:%s 顏色:%s 最高速度:%d 售價:%d 折扣價:%.0f%n", 
+				model, color, maxSpeed, price, (price * discountRate));
 	}
 	
 }
