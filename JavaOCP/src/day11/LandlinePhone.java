0955798f40f17c7e30ce57ecccc8a0b19b98caf8
FH <FH@DESKTOP-48IJ5S0>
2024-07-09 20:37:07 +0800

    java

--- JavaOCP/src/day11/LandlinePhone.java
+++ JavaOCP/src/day11/LandlinePhone.java
@@ -8,12 +8,20 @@ public class LandlinePhone extends Phone {
 	}
 	 
 	public LandlinePhone(boolean hasAnswering) {
+		this.hasAnswering = hasAnswering;
 	}
 	 
 	public LandlinePhone(String brand, String model, boolean hasAnswering) {
+		super(brand, model);
+		this.hasAnswering = hasAnswering;
 	}
 	 
 	public void recordMessage() {
+		if(hasAnswering) {
+			System.out.printf("請聽到嗶~聲後留言...");
+		} else {
+			System.out.println("本裝置不支援答錄");
+		}
 	}
 	 
 }
