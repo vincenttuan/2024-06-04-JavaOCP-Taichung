45e79cb366a550db1c6d576e13c5aecbaa2d8aee
FH <FH@DESKTOP-48IJ5S0>
2024-07-09 19:45:05 +0800

    java

--- JavaOCP/src/day11/Phone.java
+++ JavaOCP/src/day11/Phone.java
@@ -2,18 +2,21 @@ package day11;
 
 public class Phone {
 
-	private String brand;
+	private String brand; // 品牌：描述手機的製造商，如 Apple, Samsung。
 
-	private String model;
-
+	private String model; // 型號：描述手機的具體型號，如 iPhone 14, Galaxy S21。
+	
+	// 父類別的建構子
 	public Phone() {
 	}
-
+	
+	// 父類別的建構子：初始化品牌和型號
 	public Phone(String brand, String model) {
 		this.brand = brand;
 		this.model = model;
 	}
-
+	
+	// 打電話的方法: 模擬撥打電話的功能
 	public void makeCall(String number) {
 		System.out.printf("%s %s Calling: %s%n", brand, model, number);
 	}
