f2f051e3bbc2c6e6bf0969aac034e888047c6c2f
FH <FH@DESKTOP-48IJ5S0>
2024-07-09 20:04:27 +0800

    java

--- JavaOCP/src/day11/SmartPhone.java
+++ JavaOCP/src/day11/SmartPhone.java
@@ -5,15 +5,24 @@ public class SmartPhone extends Phone {
 	private String operationSystem;
 	 
 	public SmartPhone() {
+	
 	}
 	 
 	public SmartPhone(String operationSystem) {
+		this.operationSystem = operationSystem;
 	}
 	 
 	public SmartPhone(String brand, String model, String operationSystem) {
+		super(brand, model);
+		this.operationSystem = operationSystem;
 	}
 	 
 	public void installApp(String appName) {
+		System.out.printf("Install %s on %s%n", appName, operationSystem);
+	}
+	
+	public void setOperationSystem(String operationSystem) {
+		this.operationSystem = operationSystem;
 	}
 	 
 }
