08631b69f4afdab2ac19a38c8370220140d0890c
FH <FH@DESKTOP-48IJ5S0>
2024-07-09 19:43:54 +0800

    java

--- JavaOCP/src/day11/Phone.java
+++ JavaOCP/src/day11/Phone.java
@@ -1,22 +1,21 @@
 package day11;
 
 public class Phone {
- 
+
 	private String brand;
-	 
+
 	private String model;
-	 
+
 	public Phone() {
 	}
-	 
+
 	public Phone(String brand, String model) {
 		this.brand = brand;
 		this.model = model;
 	}
-	 
+
 	public void makeCall(String number) {
 		System.out.printf("%s %s Calling: %s%n", brand, model, number);
 	}
-	 
+
 }
- 
