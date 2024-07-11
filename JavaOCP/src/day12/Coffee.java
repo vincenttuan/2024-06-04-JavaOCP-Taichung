6b8704de9b01f3572b19c8790bcf6316b7889257
FH <FH@DESKTOP-48IJ5S0>
2024-07-11 18:21:11 +0800

    java

---
+++ JavaOCP/src/day12/Coffee.java
@@ -0,0 +1,18 @@
+package day12;
+
+// 父類別 Coffee
+public class Coffee {
+	int basePrice; // 咖啡價格
+	
+	public Coffee(int basePrice) { // 建構子: 設定/注入咖啡價格
+		this.basePrice = basePrice;
+	}
+	
+	public int calculatePrice() { // 計算並得到咖啡的價格
+		return basePrice;
+	}
+	
+	public void description() {
+		System.out.println("這是一杯美式咖啡");
+	}
+}
