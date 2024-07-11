17a3c1a6f6f233a4b69793a16921faff6973e1e8
FH <FH@DESKTOP-48IJ5S0>
2024-07-11 19:00:01 +0800

    java

--- JavaOCP/src/day12/Coffee.java
+++ JavaOCP/src/day12/Coffee.java
@@ -2,7 +2,8 @@ package day12;
 
 // 父類別 Coffee
 public class Coffee {
-	int basePrice; // 咖啡價格
+	//int basePrice; // 咖啡價格(同資料夾的程式可以直接取用)
+	protected int basePrice; // 咖啡價格(子類別或同資料夾的程式可以直接取用)
 	
 	// 建構子: 設定/注入咖啡價格
 	public Coffee(int basePrice) {
--- JavaOCP/src/day12/Latte.java
+++ JavaOCP/src/day12/Latte.java
@@ -4,9 +4,17 @@ package day12;
 public class Latte extends Coffee {
 	private int milkPrice; //牛奶價格
 	
-	// // 建構子: 設定/注入咖啡價格, 牛奶價格
+	// 建構子: 設定/注入咖啡價格, 牛奶價格
 	public Latte(int basePrice, int milkPrice) {
 		super(basePrice);
 		this.milkPrice = milkPrice;
+	}
+	
+	public int calculatePrice() { // 計算並得到咖啡+牛奶的價格
+		return basePrice + milkPrice;
+	}
+	
+	public void description() {
+		System.out.println("這是一杯拿鐵咖啡");
 	}
 }
