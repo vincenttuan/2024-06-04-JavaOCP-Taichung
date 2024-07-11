7e08b2e276219d0f4385e1cde6e9bae275b49ed2
FH <FH@DESKTOP-48IJ5S0>
2024-07-11 19:23:39 +0800

    java

--- JavaOCP/src/day12/CoffeeShop.java
+++ JavaOCP/src/day12/CoffeeShop.java
@@ -9,5 +9,9 @@ public class CoffeeShop {
 		Latte latte = new Latte(50, 15);
 		System.out.printf("拿鐵價格:$%d%n", latte.calculatePrice());
 		latte.description();
+		
+		Mocca mocca = new Mocca(50, 15, 10);
+		System.out.printf("摩卡價格:$%d%n", mocca.calculatePrice());
+		mocca.description();
 	}
 }
