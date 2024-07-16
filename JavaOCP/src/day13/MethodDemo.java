acc6e4498dd3f8ba5ebada0b22aa9f671177933d
FH <FH@DESKTOP-48IJ5S0>
2024-07-16 19:03:25 +0800

    java

---
+++ JavaOCP/src/day13/MethodDemo.java
@@ -0,0 +1,32 @@
+package day13;
+
+class Father {
+	
+	int x = 100;
+	
+	void play() {
+		System.out.println("Golf");
+	}
+	
+}
+
+class Son extends Father {
+	
+	int x = 200;
+	
+	void play() {
+		System.out.println("LoL");
+	}
+	
+	void printInfo() {
+		
+	}
+}
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
