582e52771b5838523447c55237f48b515ba1b832
FH <FH@DESKTOP-48IJ5S0>
2024-07-16 18:23:54 +0800

    java

---
+++ JavaOCP/src/day13/ConstructorDemo.java
@@ -0,0 +1,29 @@
+package day13;
+
+// 建構子 this() 與 super() 調用
+class Foo {
+	Foo(char c) {
+		this();
+		System.out.println(c);
+	}
+	Foo() {
+		System.out.println("A");
+	}
+}
+
+class Bar extends Foo {
+	Bar(char c) {
+		super(c);
+		System.out.println('C');
+	}
+}
+
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
