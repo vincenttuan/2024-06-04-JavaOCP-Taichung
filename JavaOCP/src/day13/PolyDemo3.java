86f9eb1f6135bd3be9176b194a6a3be3357352e3
FH <FH@DESKTOP-48IJ5S0>
2024-07-16 20:54:37 +0800

    java

---
+++ JavaOCP/src/day13/PolyDemo3.java
@@ -0,0 +1,29 @@
+package day13;
+
+public class PolyDemo3 {
+	
+	// 任意取得一個動物
+	public static Animal getAnimal(int i) {
+		switch (i) {
+			case 1:
+				return new Dog();
+			case 2:
+				return new Cat();
+			case 3:
+				return new Bird();
+			case 4:
+				return new Ostrich();
+		}
+		return new Animal();
+	}
+	
+	public static void main(String[] args) {
+		Animal animal = getAnimal(1);
+		animal.makeSound();
+		System.out.println(animal.getClass().getName());
+		System.out.println(animal.getClass().getSimpleName());
+		if(animal instanceof Dog) {
+			((Dog)animal).fetch();
+		}
+	}
+}
