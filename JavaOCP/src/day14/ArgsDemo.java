a277461eb588b319a0b1e602b7c8f5244f7cd7d9
FH <FH@DESKTOP-48IJ5S0>
2024-07-18 18:49:23 +0800

    java

---
+++ JavaOCP/src/day14/ArgsDemo.java
@@ -0,0 +1,13 @@
+package day14;
+
+public class ArgsDemo {
+	
+	// 探討 args
+	// 執行: java ArgsDemo 參數1 參數2 參數3 ... 
+	// 範例: java ArgsDemo apple banana 
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+	}
+
+}
