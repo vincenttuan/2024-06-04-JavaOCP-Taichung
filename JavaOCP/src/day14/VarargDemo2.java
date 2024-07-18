d58eb9e160f43d7e5347b7324444f2620f1f6d44
FH <FH@DESKTOP-48IJ5S0>
2024-07-18 18:38:04 +0800

    java

--- JavaOCP/src/day14/VarargDemo2.java
+++ JavaOCP/src/day14/VarargDemo2.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 public class VarargDemo2 {
 	
-	// 購物車
+	// 購物車 1
+	public static void cart() {
+		System.out.println("購物車裡面沒有商品");
+	}
+	
+	// 購物車 2
 	public static void cart(String... products) {
 		System.out.println("購物車裡面有:");
 		Arrays.stream(products)
@@ -13,9 +18,11 @@ public class VarargDemo2 {
 	
 	public static void main(String[] args) {
 		
+		cart("化妝品", "鉛筆", "零食", "筆筒");
 		cart("化妝品", "鉛筆", "零食");
 		cart("電腦", "手機");
-		
+		cart("信紙");
+		cart();
 		
 	}
 
