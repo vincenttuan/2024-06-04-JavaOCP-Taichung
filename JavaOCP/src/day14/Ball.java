4390477af34f113b0cadfb4aee10c6b6bd8cfdd7
FH <FH@DESKTOP-48IJ5S0>
2024-07-18 20:18:27 +0800

    java

---
+++ JavaOCP/src/day14/Ball.java
@@ -0,0 +1,12 @@
+package day14;
+
+public class Ball {
+	private String color;
+	private int price;
+	
+	public Ball(String color, int price) {
+		this.color = color;
+		this.price = price;
+	}
+	
+}
