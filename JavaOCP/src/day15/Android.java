72a14f3517b08baafcc96b415d95b2c87e4df23c
FH <FH@DESKTOP-48IJ5S0>
2024-07-23 19:21:44 +0800

    java

---
+++ JavaOCP/src/day15/Android.java
@@ -0,0 +1,25 @@
+package day15;
+
+public class Android implements InternetService, Phone {
+
+	@Override
+	public void connectToInternet() {
+		System.out.println("具有連網功能");
+	}
+
+	@Override
+	public void installApp(String appName) {
+		System.out.println("安裝 Android 版 " + appName + " App");
+	}
+
+	@Override
+	public void call(String number) {
+		System.out.println("打電話:" + number);
+	}
+
+	@Override
+	public void sendMessage(String message) {
+		System.out.println("接收簡訊:" + message);
+	}
+
+}
