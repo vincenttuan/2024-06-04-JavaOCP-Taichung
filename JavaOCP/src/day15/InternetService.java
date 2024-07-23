f000229c95f422cc74312623ee384a5661ae440c
FH <FH@DESKTOP-48IJ5S0>
2024-07-23 20:25:04 +0800

    java

--- JavaOCP/src/day15/InternetService.java
+++ JavaOCP/src/day15/InternetService.java
@@ -1,9 +1,8 @@
 package day15;
 
 // 網路服務功能
-public interface InternetService {
+public interface InternetService extends Service {
 	
 	void connectToInternet();
-	void installApp(String appName);
 	
 }
---
+++ JavaOCP/src/day15/Service.java
@@ -0,0 +1,5 @@
+package day15;
+
+public interface Service {
+	void installApp(String appName);
+}
