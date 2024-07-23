2eb17dc06379a55d33c21346ad58cc9007067c74
FH <FH@DESKTOP-48IJ5S0>
2024-07-23 19:16:30 +0800

    java

---
+++ JavaOCP/src/day15/InternetService.java
@@ -0,0 +1,9 @@
+package day15;
+
+// 網路服務功能
+public interface InternetService {
+	
+	void connectToInternet();
+	void installApp(String appName);
+	
+}
--- JavaOCP/src/day15/Phone.java
+++ JavaOCP/src/day15/Phone.java
@@ -1,5 +1,6 @@
 package day15;
 
+// 傳統電話功能
 public interface Phone {
 	
 	public abstract void call(String number); // public abstract 可以省略不寫
