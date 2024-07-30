e22a71ccb8777627a4361191cf4c8a83183072ed
FH <FH@DESKTOP-48IJ5S0>
2024-07-30 20:23:28 +0800

    java

---
+++ JavaOCP/src/day16/TechBook.java
@@ -0,0 +1,22 @@
+package day16;
+
+public class TechBook implements Book {
+	private String name;
+	private Integer price;
+	
+	public TechBook(String name, Integer price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Integer getPrice() {
+		return price;
+	}
+
+}
