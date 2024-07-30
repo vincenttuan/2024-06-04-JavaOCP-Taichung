8f3a36b3138a9bd16da4b643a81af467a5ebd9bf
FH <FH@DESKTOP-48IJ5S0>
2024-07-30 19:54:12 +0800

    java

---
+++ JavaOCP/src/day16/Main3.java
@@ -0,0 +1,16 @@
+package day16;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		Game game1 = new LoL("英雄聯盟");
+		Game game2 = new Tower("神魔之塔");
+		
+		game1.copyRight();
+		game2.copyRight();
+		
+		
+
+	}
+
+}
