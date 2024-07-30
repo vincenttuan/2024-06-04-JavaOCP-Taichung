5ec42dd9d9ea308e1d227dd43c7b79bec592e88a
FH <FH@DESKTOP-48IJ5S0>
2024-07-30 20:44:29 +0800

    java

--- JavaOCP/src/day16/Book.java
+++ JavaOCP/src/day16/Book.java
@@ -7,14 +7,21 @@ public interface Book {
 	
 	// Java 8 後出現了在 interface 可以有實作的方法
 	// default 方法可以給外界來調用與呼叫
-	default void printDetails() {
+	// public 可以不用寫
+	public default void printDetails() {
 		System.out.println("書名:" + getName());
 		System.out.println("價格:" + formatPrice(getPrice()));
 	}
 	
 	// private 方法只可以給該介面內部調用
+	// private 在這邊一定要寫
 	private String formatPrice(Integer price) {
+		hello();
 		return "$" + price;
+	}
+	
+	private void hello() {
+		System.out.println("Hello");
 	}
 	
 
