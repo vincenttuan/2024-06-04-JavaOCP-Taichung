d9c28719161f21e40fb494cb0dda6e1eae25bcb8
FH <FH@DESKTOP-48IJ5S0>
2024-07-30 19:32:36 +0800

    java

--- JavaOCP/src/day16/Main2.java
+++ JavaOCP/src/day16/Main2.java
@@ -1,7 +1,29 @@
 package day16;
 
 public class Main2 {
+	
 	public static void main(String[] args) {
+		Tank tank = new Tank(45);
+		Truck truck = new Truck(100, 2);
+		
+		Car[] cars = {tank, truck};
+		
+		for(Car car : cars) {
+			car.carInfo();
+			if(car instanceof Weapon) {
+				((Weapon)car).attack();
+			}
+		}
+		
+		Weapon[] weapons = {tank};
+		for(Weapon weapon : weapons) {
+			weapon.attack();
+			if(weapon instanceof Car) {
+				((Car)weapon).carInfo();
+			}
+		}
+		
 		
 	}
+	
 }
