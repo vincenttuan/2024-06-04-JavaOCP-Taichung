a1c7b134718f500cb2e77134ca2dffcb353c8807
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 19:17:30 +0800

    java

--- JavaOCP/src/day17/sales/Promotion.java
+++ JavaOCP/src/day17/sales/Promotion.java
@@ -1,6 +1,20 @@
 package day17.sales;
+
 //銷售策略
 public class Promotion {
 	private int x; // 買 x
 	private int y; // 送 y
+	
+	public Promotion(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	// 計算要付幾瓶的私有方法(演算法)
+	private int getBottles(int amount) {
+		// 計算要支付費用的瓶數 
+		int bottles = amount / (x+y) * x + amount % (x+y);
+		return bottles;
+	}
+	
 }
