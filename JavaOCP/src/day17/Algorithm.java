3aed9d8ba7e49ca4d27e57dced45456dd58bb62d
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 19:38:15 +0800

    java

--- JavaOCP/src/day17/Algorithm.java
+++ JavaOCP/src/day17/Algorithm.java
@@ -17,5 +17,19 @@ public class Algorithm {
 		int total = bottles * price;
 		System.out.println(total);
 		
+		// 問題: 很多重複變數與重複的演算法程式碼
+		// ----------------------------------------------------------
+		int x2 = 2; // 買
+		int y2 = 1; // 送
+		int price2 = 12; // 飲料價格
+		int amount2 = 7; // 買入數量
+		
+		// 計算要支付費用的瓶數 
+		int bottles2 = amount2 / (x2+y2) * x2 + amount2 % (x2+y2);
+		
+		// 計算費用
+		int total2 = bottles2 * price2;
+		System.out.println(total2);
+		
 	}
 }
