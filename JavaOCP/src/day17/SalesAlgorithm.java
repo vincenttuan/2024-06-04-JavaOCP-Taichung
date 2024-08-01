225344378e09e9b7bf205724b55560467204eeb0
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 18:50:23 +0800

    java

---
+++ JavaOCP/src/day17/SalesAlgorithm.java
@@ -0,0 +1,35 @@
+package day17;
+
+public class SalesAlgorithm {
+	private int x;
+	private int y;
+	private int price;
+	private int amount;
+	
+	public SalesAlgorithm(int x, int y, int price, int amount) {
+		this.x = x;
+		this.y = y;
+		this.price = price;
+		this.amount = amount;
+	}
+	
+	// 應付金額
+	public int getTotal() {
+		int total = getBottles() * price;
+		return total;
+	}
+	
+	// 列印帳單明細
+	public void printBill() {
+		System.out.printf("飲料: %d 元 (買 %d 送 %d) 總共買: %d 瓶 應付: %d 元%n",
+				price, x, y, amount, getTotal());
+	}
+	
+	// 計算要付幾瓶的私有方法(演算法)
+	private int getBottles() {
+		// 計算要支付費用的瓶數 
+		int bottles = amount / (x+y) * x + amount % (x+y);
+		return bottles;
+	}
+	
+}
