bd16c47f64b8979a8455417e3bcfaec46dbafcf9
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 19:16:03 +0800

    java

--- JavaOCP/src/day17/sales/Product.java
+++ JavaOCP/src/day17/sales/Product.java
@@ -3,4 +3,19 @@ package day17.sales;
 public class Product {
 	private int price;
 	private String name;
+	
+	public Product(int price, String name) {
+		this.price = price;
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
 }
