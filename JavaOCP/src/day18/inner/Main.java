27a19468385db42f4af43450a103c5479f865c53
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 21:03:11 +0800

    java

---
+++ JavaOCP/src/day18/inner/Main.java
@@ -0,0 +1,12 @@
+package day18.inner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Window win = new Window();
+		Window.Close winClose = win.new Close();
+		winClose.click();
+
+	}
+
+}
