42985f156cad79112c2d9cc1b0c3c8a22ef9c1c1
FH <FH@DESKTOP-48IJ5S0>
2024-08-01 19:33:21 +0800

    java

--- JavaOCP/src/day17/sales/Main.java
+++ JavaOCP/src/day17/sales/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		Promotion promotion3 = new Promotion(1, 1);
 		
 		// 銷售(針對商品與銷售策略)
-		SalesAlgorithm sa1 = new SalesAlgorithm(oolongTea, promotion3, 100);
+		SalesAlgorithm sa1 = new SalesAlgorithm(oolongTea, promotion3, 10);
 		sa1.printBill();
 		
 		
