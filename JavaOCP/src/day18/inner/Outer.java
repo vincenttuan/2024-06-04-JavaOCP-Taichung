58d0b65834dd4b8708fbca25ac9df8dba311e310
FH <FH@DESKTOP-48IJ5S0>
2024-08-06 18:32:46 +0800

    java

---
+++ JavaOCP/src/day18/inner/Outer.java
@@ -0,0 +1,19 @@
+package day18.inner;
+
+// 外部類
+public class Outer {
+	private int x = 7;
+	private static int sx = 9;
+	
+	// 一般內部類
+	public class Inner {
+		private int x = 77;
+		public void callMe() {
+			int x = 777;
+			System.out.println(x); // 777
+			System.out.println(this.x); // 77
+			System.out.println(Outer.this.x); // 7
+			System.out.println(Outer.sx); // 9
+		}
+	}
+}
