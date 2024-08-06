86626d798d1a7244dda9f73b00481d8f8a09c75e
FH <FH@DESKTOP-48IJ5S0>
2024-08-06 20:42:13 +0800

    java

--- JavaOCP/src/day18/static_inner/MusicPlayer.java
+++ JavaOCP/src/day18/static_inner/MusicPlayer.java
@@ -13,17 +13,21 @@ public class MusicPlayer {
 	public class Player {
 		public void play() {
 			if(name != null) {
-				System.out.println("播放" + name);
+				System.out.println("播放" + MusicPlayer.this.name);
 			}
 		}
 	}
 	
 	// 音頻格式轉換器
 	public static class AudioConverter {
+		String name = "倒帶";
 		// 將指定的音頻文件轉換成指定格式
 		// 不需要存取 MusicPlayer 的成員資料
 		public static void convert(String file, String encode) {
 			System.out.println("將" + file + "轉換成" + encode + "格式");
+		}
+		public void play() {
+			System.out.println("音頻時播放" + name);
 		}
 	}
 	
