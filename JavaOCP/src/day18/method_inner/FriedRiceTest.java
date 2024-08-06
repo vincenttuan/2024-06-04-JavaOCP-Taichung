af73f112cc7e8586d4fcf455aa04360c002666e5
FH <FH@DESKTOP-48IJ5S0>
2024-08-06 19:00:53 +0800

    java

---
+++ JavaOCP/src/day18/method_inner/FriedRiceTest.java
@@ -0,0 +1,11 @@
+package day18.method_inner;
+
+public class FriedRiceTest {
+
+	public static void main(String[] args) {
+		FriedRice friedRice = new FriedRice();
+		friedRice.prepare("蛋炒飯");
+
+	}
+
+}
