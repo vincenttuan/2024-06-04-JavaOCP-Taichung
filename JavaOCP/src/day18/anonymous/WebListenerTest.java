95fe03087fbdb19ac4ed19e62406832d4a2a1c25
FH <FH@DESKTOP-48IJ5S0>
2024-08-06 19:18:14 +0800

    java

--- JavaOCP/src/day18/anonymous/WebListenerTest.java
+++ JavaOCP/src/day18/anonymous/WebListenerTest.java
@@ -3,22 +3,19 @@ package day18.anonymous;
 public class WebListenerTest {
 
 	public static void main(String[] args) {
-		WebListener listener1 = new WebListener() {
+		// 一般方式透過自訂的 MyWebListener 來實現 WebListener
+		WebListener webListener1 = new MyWebListener();
+		webListener1.onPlay("小白");
+		
+		// 透過匿名內部類別來實現 WebListener
+		WebListener webListener2 = new WebListener() {
+			
 			@Override
 			public void onPlay(String name) {
 				System.out.println("我要監聽" + name);
 			}
 		};
-		
-		WebListener listener2 = new WebListener() {
-			@Override
-			public void onPlay(String name) {
-				System.out.println("我要觀察" + name + "目前流量");
-			}
-		};
-		
-		listener1.onPlay("小白");
-		listener2.onPlay("國道1號");
+		webListener2.onPlay("小白");
 	}
 
 }
