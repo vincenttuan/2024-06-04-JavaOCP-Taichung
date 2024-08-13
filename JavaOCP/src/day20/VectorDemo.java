86a683336af47bf05dbd1a078efa81b242ee5f6f
FH <FH@DESKTOP-48IJ5S0>
2024-08-13 21:04:53 +0800

    java

--- JavaOCP/src/day20/VectorDemo.java
+++ JavaOCP/src/day20/VectorDemo.java
@@ -5,10 +5,12 @@ import java.util.Vector;
 public class VectorDemo {
 	public static void main(String[] args) {
 		// Vector Thread-safe 集合
-		Vector<Integer> v = new Vector<>();
-		for(int i=1;i<=1;i++) {
+		Vector<Integer> v = new Vector<>(41, 5);
+		for(int i=1;i<=41;i++) {
 			v.add(i);
 		}
+		v.add(100);
+		
 		System.out.println("capacity 總共的空間:" + v.capacity());
 		System.out.println("size 已用空間:" + v.size());
 		System.out.println(v);
