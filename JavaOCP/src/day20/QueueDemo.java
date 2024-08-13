48646381a6e1aeaa77a18dda5d1f53ef571bd0cd
FH <FH@DESKTOP-48IJ5S0>
2024-08-13 20:06:49 +0800

    java

---
+++ JavaOCP/src/day20/QueueDemo.java
@@ -0,0 +1,27 @@
+package day20;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+	
+	public static void main(String[] args) {
+		// 先進先出 FIFO
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(100);
+		queue.offer(80);
+		queue.offer(90);
+		System.out.println(queue);
+		
+		while (!queue.isEmpty()) {
+			// 取元素出來
+			Integer data = queue.poll();
+			System.out.println("取出:" + data);
+		}
+		
+		System.out.println(queue);
+		
+	}
+	
+	
+}
