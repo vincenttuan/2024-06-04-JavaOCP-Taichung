0a3a7a0e3faa610b558b24240c17d81a4d4d0688
FH <FH@DESKTOP-48IJ5S0>
2024-08-15 19:27:18 +0800

    java

--- JavaOCP/src/day21/PredicateDemo.java
+++ JavaOCP/src/day21/PredicateDemo.java
@@ -5,7 +5,8 @@ import java.util.function.Predicate;
 public class PredicateDemo {
 	public static void main(String[] args) {
 		// 利用 Lambda 實現 Predicate
-		Predicate<Double> predicate1 = (Double score) -> score >= 60;
+		//Predicate<Double> predicate1 = (Double score) -> score >= 60;
+		Predicate<Double> predicate1 = (score) -> score >= 60;
 		System.out.println(predicate1.test(85.0));
 		System.out.println(predicate1.test(45.0));
 		
