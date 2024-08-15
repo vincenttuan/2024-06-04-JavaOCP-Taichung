9986aae72c49ef4db054201406e4afed2a477103
FH <FH@DESKTOP-48IJ5S0>
2024-08-15 19:40:25 +0800

    java

--- JavaOCP/src/day21/SupplierDemo.java
+++ JavaOCP/src/day21/SupplierDemo.java
@@ -1,16 +1,29 @@
 package day21;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.Supplier;
 
 public class SupplierDemo {
 
 	public static void main(String[] args) {
-		// 利用 Lambda 實現 Supplier
+		// 利用 Lambda 實現 Supplier (單行實作)
 		Supplier<Date> today = () -> new Date();
 		System.out.println(today.get());
 		
-		
+		// 利用 Lambda 實現 Supplier (多行實作)
+		// yyyy-MM-dd HH:mm:ss 的呈現格式
+		Supplier<String> today2 = () -> {
+			//String pattern = "HH:mm:ss";
+			//String pattern = "yyyy-MM-dd";
+			//String pattern = "yyyy-MM-dd HH:mm:ss E";
+			//String pattern = "yyyy-MM-dd a hh:mm:ss";
+			String pattern = "yyyy-MM-dd HH:mm:ss";
+			SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+			String todayStr = sdf.format(new Date());
+			return todayStr;
+		};  
+		System.out.println(today2.get());
 	}
 
 }
