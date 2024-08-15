2cd3e716c54d482c59a019b09c741d946f658b51
FH <FH@DESKTOP-48IJ5S0>
2024-08-15 19:09:31 +0800

    java

--- JavaOCP/src/day21/ConsumerDemo2.java
+++ JavaOCP/src/day21/ConsumerDemo2.java
@@ -8,10 +8,10 @@ public class ConsumerDemo2 {
 		// 利用 Lambda 來實現
 		//Consumer<Integer> consumer1 = (Integer t) -> System.out.println("數字:" + t);
 		//Consumer<String> consumer2 = (String t) -> System.out.println("字串:" + t);
-		//Consumer<Integer> consumer1 = (t) -> System.out.println("數字:" + t);
-		//Consumer<String> consumer2 = (t) -> System.out.println("字串:" + t);
-		Consumer<Integer> consumer1 = t -> System.out.println("數字:" + t);
-		Consumer<String> consumer2 = t -> System.out.println("字串:" + t);
+		Consumer<Integer> consumer1 = (t) -> System.out.println("數字:" + t);
+		Consumer<String> consumer2 = (t) -> System.out.println("字串:" + t);
+		//Consumer<Integer> consumer1 = t -> System.out.println("數字:" + t);
+		//Consumer<String> consumer2 = t -> System.out.println("字串:" + t);
 		
 		consumer1.accept(10);
 		consumer2.accept("Java");
