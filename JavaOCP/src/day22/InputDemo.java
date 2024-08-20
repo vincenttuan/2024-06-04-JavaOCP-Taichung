45174cf9f30f03f0e835ec964aba3d5ed6f30d9c
FH <FH@DESKTOP-48IJ5S0>
2024-08-20 19:05:24 +0800

    java

--- JavaOCP/src/day22/InputDemo.java
+++ JavaOCP/src/day22/InputDemo.java
@@ -1,5 +1,6 @@
 package day22;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputDemo {
@@ -12,9 +13,18 @@ public class InputDemo {
 	public static void input() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("請輸入半徑:");
-		int r = scanner.nextInt();
-		double area = r*r*Math.PI;
-		System.out.printf("半徑: %d 圓面積: %.1f%n", r, area);
+		try {
+			int r = scanner.nextInt();
+			double area = r*r*Math.PI;
+			System.out.printf("半徑: %d 圓面積: %.1f%n", r, area);
+		} catch (InputMismatchException e) {
+			System.err.println("輸入發生錯誤"); // 自訂訊息
+			System.err.println("錯誤訊息1:" + e); // 錯誤訊息1
+			System.err.println("錯誤訊息2:" + e.getMessage()); // 錯誤訊息2
+			System.err.println("錯誤類型:" + e.getCause()); // 錯誤類型
+			System.err.println("詳細錯誤資訊:");
+			e.printStackTrace(System.err); // 詳細錯誤資訊
+		}
 		scanner.close();
 	}
 
