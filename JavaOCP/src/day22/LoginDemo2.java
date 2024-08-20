4cbeb2542aee81d3d42530e12adfb86d17131a90
FH <FH@DESKTOP-48IJ5S0>
2024-08-20 20:37:15 +0800

    java

---
+++ JavaOCP/src/day22/LoginDemo2.java
@@ -0,0 +1,38 @@
+package day22;
+
+import java.util.Map;
+
+public class LoginDemo2 {
+	private static Map<String, String> userMap = Map.of("john", "1234", "mary", "5678");
+	
+	public static void main(String[] args) {
+		System.out.println(userMap);
+		try {
+			boolean loginCheck = login("john", "12345");
+			System.out.println("登入: " + loginCheck);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	// 登入
+	public static boolean login(String username, String password) throws Exception {
+		// 判斷是否有此使用者
+		if(userMap.containsKey(username)) { // userMap 是否有此使用者
+			if(userMap.get(username).equals(password)) {
+				return true;
+			} else {
+				//return false; // username 對, password 不對
+				Exception re = new Exception("password 不對"); // 自行建立一個錯誤物件
+				throw re; // 將此錯誤拋出
+			}
+		} else {
+			//return false; // username 不對
+			Exception re = new Exception("username 不對"); // 自行建立一個錯誤物件
+			throw re; // 將此錯誤拋出
+		}
+		
+	}
+
+}
