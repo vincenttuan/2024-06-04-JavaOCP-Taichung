b19261b8d4fd88a93c8ad01b5e92addffea281c9
FH <FH@DESKTOP-48IJ5S0>
2024-08-20 20:24:31 +0800

    java

---
+++ JavaOCP/src/day22/LoginDemo.java
@@ -0,0 +1,28 @@
+package day22;
+
+import java.util.Map;
+
+public class LoginDemo {
+	private static Map<String, String> userMap = Map.of("john", "1234", "mary", "5678");
+	
+	public static void main(String[] args) {
+		boolean loginCheck = login("john", "1234");
+		System.out.println("登入: " + loginCheck);
+	}
+	
+	// 登入
+	public static boolean login(String username, String password) {
+		// 判斷是否有此使用者
+		if(userMap.containsKey(username)) { // userMap 是否有此使用者
+			if(userMap.get(username).equals(password)) {
+				return true;
+			} else {
+				return false; // username 對, password 不對
+			}
+		} else {
+			return false; // username 不對
+		}
+		
+	}
+
+}
