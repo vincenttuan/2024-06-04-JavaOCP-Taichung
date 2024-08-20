a6701824e6525df577ca019b6d5e9f5932833ffc
FH <FH@DESKTOP-48IJ5S0>
2024-08-20 19:43:55 +0800

    java

--- JavaOCP/src/day22/ReadFileDemo.java
+++ JavaOCP/src/day22/ReadFileDemo.java
@@ -1,8 +1,16 @@
 package day22;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ReadFileDemo {
 
 	public static void main(String[] args) {
+		Path path = Paths.get("src/day22", "data.txt");
+		System.out.printf("檔案路徑: %s%n", path);
+		String content = Files.readString(path);
+		System.out.printf("檔案內容: %s%n", content);
 		
 		
 	}
