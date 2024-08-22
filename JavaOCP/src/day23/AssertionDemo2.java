22d0a689b06496f2c10d8892085ccb2d9e58f9fa
FH <FH@DESKTOP-48IJ5S0>
2024-08-22 18:51:03 +0800

    java

--- JavaOCP/src/day23/AssertionDemo2.java
+++ JavaOCP/src/day23/AssertionDemo2.java
@@ -3,6 +3,8 @@ package day23;
 public class AssertionDemo2 {
 
 	public static void main(String[] args) {
+		// 執行此程式一定要加上 -ea
+		// 否則系統或"掠過"此行不執行
 		assert(false); 
 		System.out.println("Hello World");
 
