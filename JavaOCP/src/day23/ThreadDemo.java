219e63220a09538dab5a81033b758145ada36481
FH <FH@DESKTOP-48IJ5S0>
2024-08-22 21:07:25 +0800

    java

--- JavaOCP/src/day23/ThreadDemo.java
+++ JavaOCP/src/day23/ThreadDemo.java
@@ -27,11 +27,21 @@ public class ThreadDemo {
 			System.out.printf("現在時刻: %s%n", sdf.format(tody));
 		};
 		
+		// 背景音樂
+		Runnable music = () -> {
+			for(int i=1;;i++) {
+				System.out.printf("背景音樂: %d%n", i);
+			}
+		};
+		
 		// 建立二條執行緒
 		Thread t1 = new Thread(lotto); // 執行 Lotto 數字工作
 		Thread t2 = new Thread(now); // 執行 現在時刻工作
+		Thread t3 = new Thread(music); // 執行 背景音樂
+		t3.setDaemon(true); // 將 t3 設定為背景執行緒
 		
 		// 啟動
+		t3.start();
 		t1.start();
 		t2.start();
 		
