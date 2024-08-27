a621c76f13b9cd1edbb23a6f541b6bde5b2d9834
FH <FH@DESKTOP-48IJ5S0>
2024-08-27 19:54:59 +0800

    java

--- JavaOCP/src/day24/Account.java
+++ JavaOCP/src/day24/Account.java
@@ -11,10 +11,12 @@ public class Account {
 	public void withdraw(int amount) {
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 要提款$ %d%n", tName, amount);
-		if(balance >= amount) {
-			int newBalance = balance - amount; // 新的帳戶餘額
+		// 取得目前最新餘額
+		int currentBalance= balance;
+		if(currentBalance >= amount) {
+			int newBalance = currentBalance - amount; // 新的帳戶餘額
 			// 模擬提款程序所花費的時間
-			for(int i=1;i<=1000;i++) {
+			for(int i=1;i<=10000;i++) {
 				// 模擬提款程序
 			}
 			balance = newBalance; // 更新帳戶餘額
