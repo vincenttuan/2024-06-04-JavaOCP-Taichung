d11fece4421968d207a9af2cd27873f9142846ff
FH <FH@DESKTOP-48IJ5S0>
2024-08-29 20:21:28 +0800

    java

--- JavaOCP/src/day25/ParallelDemo.java
+++ JavaOCP/src/day25/ParallelDemo.java
@@ -9,13 +9,14 @@ public class ParallelDemo {
 		// 循序運算(印出每一個元素的平方)
 		List<Integer> nums = List.of(2, 4, 6, 8, 10);
 		nums.stream()
+			.sequential() // 預設(循序流)可以不用寫
 			.mapToInt(n -> n * n)
 			.forEach(n -> System.out.println(n));
 		
 		System.out.println("------");
 		
 		// 平行運算
-		int parallelism = (ForkJoinPool.getCommonPoolParallelism());
+		int parallelism = ForkJoinPool.getCommonPoolParallelism();
 		System.out.printf("此電腦可以同步執行的任務數量(並行度): %d%n", parallelism);
 		nums.parallelStream()
 			.mapToInt(n -> n * n)
