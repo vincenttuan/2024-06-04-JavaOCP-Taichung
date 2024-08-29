ad4ae5d6885413450c48a725d4800f616d9c0ee8
FH <FH@DESKTOP-48IJ5S0>
2024-08-29 19:21:02 +0800

    java

---
+++ JavaOCP/src/day25/TaskDemo.java
@@ -0,0 +1,27 @@
+package day25;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TaskDemo {
+
+	public static void main(String[] args) {
+		// 建立任務
+		Runnable r1 = new ShortTask(); // 短任務
+		Runnable r2 = new ShortTask(); // 短任務
+		Runnable r3 = new LongTask();  // 長任務
+		Runnable r4 = new LongTask();  // 長任務
+		Runnable r5 = new ShortTask(); // 短任務
+		
+		// 建立一個執行緒服務
+		ExecutorService service = Executors.newCachedThreadPool();
+		service.submit(r1); // 啟動短任務
+		service.submit(r2); // 啟動短任務
+		service.submit(r3); // 啟動長任務
+		service.submit(r4); // 啟動長任務
+		service.submit(r5); // 啟動短任務
+		
+		
+	}
+
+}
