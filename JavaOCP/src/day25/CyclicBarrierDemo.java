39f26e8dca7c827426f618c36fb327de9d97c8a8
FH <FH@DESKTOP-48IJ5S0>
2024-08-29 20:42:15 +0800

    java

--- JavaOCP/src/day25/CyclicBarrierDemo.java
+++ JavaOCP/src/day25/CyclicBarrierDemo.java
@@ -1,6 +1,8 @@
 package day25;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // 循環屏障
 public class CyclicBarrierDemo {
@@ -18,6 +20,25 @@ public class CyclicBarrierDemo {
 		Runnable car3 = new Car(cyclicBarrier);
 		Runnable car4 = new Car(cyclicBarrier);
 		
+		/** 傳統寫法
+		Thread t1 = new Thread(car1);
+		Thread t2 = new Thread(car1);
+		Thread t3 = new Thread(car1);
+		Thread t4 = new Thread(car1);
+		t1.start();
+		t2.start();
+		t3.start();
+		t3.start();
+		*/
+		
+		/** 使用執行緒服務框架寫法 */
+		ExecutorService service = Executors.newCachedThreadPool();
+		service.submit(car1);
+		service.submit(car2);
+		service.submit(car3);
+		service.submit(car4);
+		
+		service.shutdown();
 		
 		
 	}
