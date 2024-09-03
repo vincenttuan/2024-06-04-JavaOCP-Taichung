66a5bd1e51f8b66add238127112191f09e68c157
FH <FH@DESKTOP-48IJ5S0>
2024-09-03 19:00:00 +0800

    java

--- JavaOCP/src/day26/NIO_ReadFile.java
+++ JavaOCP/src/day26/NIO_ReadFile.java
@@ -9,7 +9,7 @@ public class NIO_ReadFile {
 	public static void main(String[] args) throws IOException {
 		Path filePath = Path.of("src", "day26", "data.txt");
 		// 讀取檔案內容
-		String data = Files.readString(filePath);
+		String data = Files.readString(filePath); // Java 11 語法
 		System.out.println(data);
 	}
 
