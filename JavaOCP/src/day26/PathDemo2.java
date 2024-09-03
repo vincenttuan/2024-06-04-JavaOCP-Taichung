fcb1fc66d7ae0a76e2c2660168e828bba01dcd68
FH <FH@DESKTOP-48IJ5S0>
2024-09-03 19:31:16 +0800

    java

--- JavaOCP/src/day26/PathDemo2.java
+++ JavaOCP/src/day26/PathDemo2.java
@@ -8,7 +8,8 @@ public class PathDemo2 {
 	public static void main(String[] args) {
 		// resolve() 路徑結合
 		Path path1 = Paths.get("C:/temp/company/finance");
-		Path path2 = Paths.get("salary.txt");
+		//Path path2 = Paths.get("salary.txt"); // 相對結合
+		Path path2 = Paths.get("/salary.txt"); // 絕對結合
 		Path path3 = path1.resolve(path2);
 		System.out.println(path1);
 		System.out.println(path2);
