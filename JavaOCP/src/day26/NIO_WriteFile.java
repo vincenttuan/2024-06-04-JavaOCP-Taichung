e9399c87bffe93ff628e715c026d7871d12fc6ea
FH <FH@DESKTOP-48IJ5S0>
2024-09-03 19:08:49 +0800

    java

--- JavaOCP/src/day26/NIO_WriteFile.java
+++ JavaOCP/src/day26/NIO_WriteFile.java
@@ -3,14 +3,15 @@ package day26;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class NIO_WriteFile {
 
 	public static void main(String[] args) throws IOException {
 		String data = "\nHappy time!";
 		Path filePath = Path.of("src", "day26", "data.txt");
-		
-		Files.writeString(filePath, data);
+		// StandardOpenOption.APPEND 寫入到原資料的後面
+		Files.writeString(filePath, data, StandardOpenOption.APPEND);
 		System.out.println("寫檔成功");
 	}
 
