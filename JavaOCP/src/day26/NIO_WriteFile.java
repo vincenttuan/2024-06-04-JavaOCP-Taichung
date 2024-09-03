b3c89631811c84648159995fc73fd64596b2b10d
FH <FH@DESKTOP-48IJ5S0>
2024-09-03 19:05:09 +0800

    java

---
+++ JavaOCP/src/day26/NIO_WriteFile.java
@@ -0,0 +1,18 @@
+package day26;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIO_WriteFile {
+
+	public static void main(String[] args) throws IOException {
+		String data = "\nHappy time!";
+		Path filePath = Path.of("src", "day26", "data.txt");
+		
+		Files.writeString(filePath, data);
+		System.out.println("寫檔成功");
+	}
+
+}
