42ee78defdf8b3c63b283092930a29c0db265de4
FH <FH@DESKTOP-48IJ5S0>
2024-09-03 18:50:13 +0800

    java

---
+++ JavaOCP/src/day26/IO_ReadFile.java
@@ -0,0 +1,26 @@
+package day26;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IO_ReadFile {
+
+	public static void main(String[] args) {
+		char[] buffer = new char[1]; // 讀資料暫存區(一次讀一個字元)
+		
+		try(FileReader fr = new FileReader("src/day26/data.txt");) {
+			
+			System.out.println("讀取 data.txt 檔案內容");
+			System.out.println("------------------");
+			
+			while (fr.read(buffer) != -1) {
+				System.out.print(buffer[0]);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+
+	}
+
+}
