4956ba069470ba648786207001be3a3f8a943a8c
FH <FH@DESKTOP-48IJ5S0>
2024-09-12 19:55:22 +0800

    web

--- JavaWeb/src/main/java/servlet/BeverageServlet.java
+++ JavaWeb/src/main/java/servlet/BeverageServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.entity.Beverage;
 
 @WebServlet("/servlet/beverage")
 public class BeverageServlet extends HttpServlet {
@@ -37,8 +38,15 @@ public class BeverageServlet extends HttpServlet {
 		// 計算飲料價格(預設飲料一杯 30 元)
 		int totalPrice = 30 * Integer.parseInt(amount);
 		
-		// 傳遞給 jsp
+		// 建立 entity 用來存放要傳遞給 jsp 的資料
+		Beverage beverage = new Beverage(userName, email, orderDate, amount, beverageName, 
+										 ice, sugar, additionalArray, memo, totalPrice);
 		
+		// 將 beverage 放到 request 屬性中, 目的是要傳遞給 jsp 使用
+		req.setAttribute("beverage", beverage);
+		
+		// forward 到指定 jsp
+		req.getRequestDispatcher("/WEB-INF/view/beverage_form.jsp").forward(req, resp); 
 		
 	}
 	
