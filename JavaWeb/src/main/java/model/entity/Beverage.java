bbd462ffb4245022887302ccabea607621c456b0
FH <FH@DESKTOP-48IJ5S0>
2024-09-12 19:47:54 +0800

    web

--- JavaWeb/src/main/java/model/entity/Beverage.java
+++ JavaWeb/src/main/java/model/entity/Beverage.java
@@ -2,6 +2,18 @@ package model.entity;
 
 // 飲料實體(entity)
 public class Beverage {
+	// 原始資料
+	private String userName;
+	private String email;
+	private String orderDate;
+	private String amount;
+	private String beverageName;
+	private String ice;
+	private String sugar;
+	private String[] additionalArray;
+	private String memo;
+	// 加值資料(計算飲料價格)
+	private int totalPrice;  
 	
 	
 }
