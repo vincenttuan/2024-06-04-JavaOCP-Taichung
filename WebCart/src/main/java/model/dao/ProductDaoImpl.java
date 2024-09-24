d4f6c27bc3f0c9cc7cf762ad80a50590701d044e
FH <FH@DESKTOP-48IJ5S0>
2024-09-24 20:48:18 +0800

    cart

--- WebCart/src/main/java/model/dao/ProductDaoImpl.java
+++ WebCart/src/main/java/model/dao/ProductDaoImpl.java
@@ -1,11 +1,27 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 import model.entity.Product;
 
 public class ProductDaoImpl implements ProductDao {
-
+	private static Connection conn;
+	static {
+		try {
+			// 1.建立 Driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// 2.建立連線 Connection
+			String username = "root";
+			String password = "12345678";
+			String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei";
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void add(Product product) {
 		// TODO Auto-generated method stub
