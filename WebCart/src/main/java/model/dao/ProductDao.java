8998b5710a2aec6dadb8f6bb667e0c032077508c
FH <FH@DESKTOP-48IJ5S0>
2024-09-24 20:36:04 +0800

    cart

--- WebCart/src/main/java/model/dao/ProductDao.java
+++ WebCart/src/main/java/model/dao/ProductDao.java
@@ -1,5 +1,20 @@
 package model.dao;
 
-public interface ProductDao {
+import java.util.List;
 
+import model.entity.Product;
+
+// product 資料表 CRUD
+public interface ProductDao {
+	void add(Product product); // 新增一筆紀錄
+	
+	void update(Integer productId, Product newProduct); // 修改紀錄, 根據指定 productId
+	void updateQty(Integer productId, Integer newQty); // 修改庫存紀錄, 根據指定 productId
+	
+	void delete(Integer productId); // 刪除庫存紀錄, 根據指定 productId
+	
+	List<Product> findAll(); // 全部查詢
+	
+	Product getOne(Integer productId); // 單筆查詢
+	
 }
