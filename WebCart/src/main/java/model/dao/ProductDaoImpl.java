286b502b40b5d2e5099fd649adf2f4051a55a9a1
FH <FH@DESKTOP-48IJ5S0>
2024-09-24 20:42:15 +0800

    web

--- WebCart/src/main/java/model/dao/ProductDaoImpl.java
+++ WebCart/src/main/java/model/dao/ProductDaoImpl.java
@@ -1,5 +1,45 @@
 package model.dao;
 
-public class ProductDaoImpl {
+import java.util.List;
+
+import model.entity.Product;
+
+public class ProductDaoImpl implements ProductDao {
+
+	@Override
+	public void add(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Integer productId, Product newProduct) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateQty(Integer productId, Integer newQty) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer productId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getOne(Integer productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
