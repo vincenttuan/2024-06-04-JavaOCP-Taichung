b31584b424d0294bad6f92ed1464c9ccb512fc6c
FH <FH@DESKTOP-48IJ5S0>
2024-09-24 21:08:13 +0800

    cart

--- WebCart/src/main/java/model/dao/ProductDaoImpl.java
+++ WebCart/src/main/java/model/dao/ProductDaoImpl.java
@@ -2,6 +2,10 @@ package model.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.Product;
@@ -50,8 +54,31 @@ public class ProductDaoImpl implements ProductDao {
 
 	@Override
 	public List<Product> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select product_id, product_name, price, qty from product";
+		List<Product> products = new ArrayList<>();
+		try(Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);) {
+			// 利用 while 逐筆讀取資料
+			while (rs.next()) {
+				Integer productId = rs.getInt("product_id");
+				String productName = rs.getString("product_name");
+				Integer price = rs.getInt("price");
+				Integer qty = rs.getInt("qty");
+				// 建立 Product 物件
+				Product product = new Product();
+				product.setProductId(productId);
+				product.setProductName(productName);
+				product.setPrice(price);
+				product.setQty(qty);
+				// 注入到 products 集合中保存
+				products.add(product);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return products;
 	}
 
 	@Override
