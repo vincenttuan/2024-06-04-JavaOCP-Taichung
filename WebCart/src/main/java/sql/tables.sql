774b3b9e5018d1ce8fa9241543528320f208829d
FH <FH@DESKTOP-48IJ5S0>
2024-10-01 18:49:08 +0800

    web

--- WebCart/src/main/java/sql/tables.sql
+++ WebCart/src/main/java/sql/tables.sql
@@ -21,7 +21,7 @@ create table if not exists product (
 	product_id int primary key comment '商品ID',
     product_name varchar(50) not null unique comment '商品名稱',
     price int not null comment '商品價格',
-    qty int default 0 comment '商品庫存',
+    stock_quantity int default 0 comment '商品庫存',
     image_base64 longtext comment '商品照片'
 );
 
