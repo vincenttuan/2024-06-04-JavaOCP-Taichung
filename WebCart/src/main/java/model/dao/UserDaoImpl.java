46de16d593771c692ef4965e61000a316257f37d
FH <FH@DESKTOP-48IJ5S0>
2024-10-15 19:26:11 +0800

    web

---
+++ WebCart/src/main/java/model/dao/UserDaoImpl.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entity.User;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
