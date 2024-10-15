60dace1791c813aa68cafb893d4f53e6484be81e
FH <FH@DESKTOP-48IJ5S0>
2024-10-15 19:27:32 +0800

    web

--- WebCart/src/main/java/model/dao/UserDaoImpl.java
+++ WebCart/src/main/java/model/dao/UserDaoImpl.java
@@ -1,11 +1,29 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 import model.entity.User;
 
 public class UserDaoImpl implements UserDao {
-
+	private static Connection conn;
+	
+	static {
+		try {
+			// 1.建立 Driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// 2.建立連線 Connection
+			String username = "root";
+			String password = "12345678";
+			String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei";
+			conn = DriverManager.getConnection(dbUrl, username, password);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public List<User> findAllUsers() {
 		// TODO Auto-generated method stub
