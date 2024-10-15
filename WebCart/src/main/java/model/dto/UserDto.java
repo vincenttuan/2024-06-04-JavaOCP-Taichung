f16ddf7e5d1e323de8c19bbd29c7b2b7ef24998d
FH <FH@DESKTOP-48IJ5S0>
2024-10-15 19:23:34 +0800

    web

---
+++ WebCart/src/main/java/model/dto/UserDto.java
@@ -0,0 +1,29 @@
+package model.dto;
+
+public class UserDto {
+	private Integer userId;
+	private String username;
+	private Integer priority;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	
+	
+	
+}
