b62b619a35e18346648edd7fbab9cc4d36dae5af
FH <FH@DESKTOP-48IJ5S0>
2024-10-17 20:00:43 +0800

    web

--- WebCart/src/main/java/controller/LoginServlet.java
+++ WebCart/src/main/java/controller/LoginServlet.java
@@ -7,9 +7,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.dto.UserDto;
+import model.service.UserService;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+	
+	private UserService userService = new UserService();
 	
 	// 呈現登入頁面給使用者
 	@Override
@@ -20,7 +24,13 @@ public class LoginServlet extends HttpServlet {
 	// 驗證使用者登入
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		// 接收登入表單的資訊
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		// 登入比對
+		UserDto userDto = userService.loginCheck(username, password);
+		String result = userDto == null ? "login error" : "login success";
+		resp.getWriter().print(result);
 	}
 	
 	
