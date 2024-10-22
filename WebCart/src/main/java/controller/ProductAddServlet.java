1aebcf33df9e8785b46dfd5e20513d0947cc10fb
FH <FH@DESKTOP-48IJ5S0>
2024-10-22 19:20:23 +0800

    web

--- WebCart/src/main/java/controller/ProductAddServlet.java
+++ WebCart/src/main/java/controller/ProductAddServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.Part;
 import model.service.ProductService;
 
