27c532244712a73187c209c12aab1f3d91d82182
FH <FH@DESKTOP-48IJ5S0>
2024-10-22 18:55:22 +0800

    web

--- WebCart/src/main/java/controller/LogoutServlet.java
+++ WebCart/src/main/java/controller/LogoutServlet.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.io.IOException;
 
-import com.mysql.cj.Session;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
