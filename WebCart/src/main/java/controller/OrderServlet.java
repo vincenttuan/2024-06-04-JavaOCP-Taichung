fcd363ff4a89a56c8a1ceda2501c898232e0247c
FH <FH@DESKTOP-48IJ5S0>
2024-10-24 20:31:07 +0800

    web

---
+++ WebCart/src/main/java/controller/OrderServlet.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.dto.ProductDto;
+import model.service.ProductService;
+
+@WebServlet("/order")
+public class OrderServlet extends HttpServlet {
+	
+	private ProductService productService = new ProductService();
+	
+	// 訂購頁面
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<ProductDto> productDtos = productService.queryAll();
+		req.setAttribute("productDtos", productDtos);
+		req.getRequestDispatcher("/WEB-INF/view/order.jsp").forward(req, resp);
+	}
+	
+	// 訂購程序->到購物車 (存入到資料表中)
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
