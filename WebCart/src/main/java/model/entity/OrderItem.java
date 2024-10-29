a647d807bb18e4456c34757ec9888f9a41ad5bb7
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 18:46:02 +0800

    web

--- WebCart/src/main/java/model/entity/OrderItem.java
+++ WebCart/src/main/java/model/entity/OrderItem.java
@@ -1,5 +1,42 @@
 package model.entity;
 
 public class OrderItem {
-
+	private Integer itemId;
+	private Integer orderId;
+	private Integer productId;
+	private Integer quantity; // 購買數量
+	private Integer unitPrice; // 單價
+	
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	
 }
