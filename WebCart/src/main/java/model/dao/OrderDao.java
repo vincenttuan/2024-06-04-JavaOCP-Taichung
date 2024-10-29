2fee1018090765ed92c395a10d0014952c7285bb
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 18:55:45 +0800

    web

--- WebCart/src/main/java/model/dao/OrderDao.java
+++ WebCart/src/main/java/model/dao/OrderDao.java
@@ -2,9 +2,20 @@ package model.dao;
 
 import java.util.List;
 
+import model.entity.Order;
+import model.entity.OrderItem;
+
 public interface OrderDao {
-	// 取得所有訂單
-	public List<Order> findAllOrders() {
-		
-	}
+	// 取得該用戶所有訂單主檔
+	public List<Order> findAllOrdersByUserId(Integer userId);
+	
+	// 取得單訂單的所有明細
+	public List<OrderItem> findAllOrderItemsByOrderId(Integer orderId);
+	
+	// 新增訂單(主檔)會得到訂單ID(order_id)
+	public Integer addOrder(Order order);
+	
+	// 新增訂單明細
+	public void addItem(OrderItem orderItem);
+	
 }
