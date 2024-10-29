085a9012b761548a3183254540daa373ebb42fa4
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 19:17:59 +0800

    web

--- WebCart/src/main/java/model/dao/OrderDaoImpl.java
+++ WebCart/src/main/java/model/dao/OrderDaoImpl.java
@@ -3,6 +3,7 @@ package model.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.Order;
@@ -12,20 +13,31 @@ public class OrderDaoImpl extends BaseDao implements OrderDao {
 
 	@Override
 	public List<Order> findAllOrdersByUserId(Integer userId) {
+		List<Order> orders = new ArrayList<>();
 		String sql = "select order_id, user_id, order_date, total_price, order_status from orders where user_id = ?";
 		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			
 			pstmt.setInt(1, userId);
 			
 			try(ResultSet rs = pstmt.executeQuery()) {
-				
+				// 逐筆將每一筆紀錄放到 Order 物件中
+				while (rs.next()) {
+					Order order = new Order();
+					order.setOrderId(rs.getInt("order_id"));
+					order.setUserId(rs.getInt("user_id"));
+					order.setOrderDate(rs.getString("order_date"));
+					order.setTotalPrice(rs.getDouble("total_price"));
+					order.setOrderStatus(rs.getString("order_status"));
+					// 加入到 orders 集合中
+					orders.add(order);
+				}
 			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
-		return null;
+		return orders;
 	}
 
 	@Override
