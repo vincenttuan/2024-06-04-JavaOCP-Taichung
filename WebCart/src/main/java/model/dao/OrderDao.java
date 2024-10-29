23d030739800d10b2cb75f244c31793cae6c594e
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 18:57:07 +0800

    web

--- WebCart/src/main/java/model/dao/OrderDao.java
+++ WebCart/src/main/java/model/dao/OrderDao.java
@@ -18,4 +18,6 @@ public interface OrderDao {
 	// 新增訂單明細
 	public void addItem(OrderItem orderItem);
 	
+	// 變更訂單狀態(Finished(結帳), Pending, Cancel(取消))
+	public void changeOrderStatus(String orderStatus);
 }
