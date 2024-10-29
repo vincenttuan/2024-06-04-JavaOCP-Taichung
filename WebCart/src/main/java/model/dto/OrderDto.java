2e9696ca2e24af3c36f62a90b8a905e77d5a2d3a
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 20:53:35 +0800

    web

--- WebCart/src/main/java/model/dto/OrderDto.java
+++ WebCart/src/main/java/model/dto/OrderDto.java
@@ -3,7 +3,7 @@ package model.dto;
 public class OrderDto {
 	private Integer orderId;
 	private Integer userId;
-	private String username;
+	private String username; // 利用 userId 可查到
 	private String orderDate;
 	private Double totalPrice;
 	private String orderStatus;
---
+++ WebCart/src/main/java/model/dto/OrderItemDto.java
@@ -0,0 +1,35 @@
+package model.dto;
+
+public class OrderItemDto {
+	private Integer itemId;
+	private String productName; // 利用 productId 可查到
+	private Integer quantity;
+	private Double unitPrice;
+	
+	public Integer getItemId() {
+		return itemId;
+	}
+	public void setItemId(Integer itemId) {
+		this.itemId = itemId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	
+	
+}
