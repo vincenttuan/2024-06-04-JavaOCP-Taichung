fbb7650e824657241f137d509c4700d03eeea2c0
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 19:12:24 +0800

    web

--- WebCart/src/main/java/model/dao/OrderDaoImpl.java
+++ WebCart/src/main/java/model/dao/OrderDaoImpl.java
@@ -1,5 +1,8 @@
 package model.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.entity.Order;
@@ -9,7 +12,19 @@ public class OrderDaoImpl extends BaseDao implements OrderDao {
 
 	@Override
 	public List<Order> findAllOrdersByUserId(Integer userId) {
-		// TODO Auto-generated method stub
+		String sql = "select order_id, user_id, order_date, total_price, order_status from orders where user_id = ?";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, userId);
+			
+			try(ResultSet rs = pstmt.executeQuery()) {
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
