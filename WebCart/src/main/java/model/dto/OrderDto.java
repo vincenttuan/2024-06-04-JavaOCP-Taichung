150a462ec6c2b15be673bb41109a1011303ad385
FH <FH@DESKTOP-48IJ5S0>
2024-10-29 20:51:39 +0800

    web

---
+++ WebCart/src/main/java/model/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package model.dto;
+
+public class OrderDto {
+	private Integer orderId;
+	private Integer userId;
+	private String username;
+	private String orderDate;
+	private Double totalPrice;
+	private String orderStatus;
+	
+	
+	
+}
