e6b3a480666b1adf0ebf100fe2c4d997d40c1280
FH <FH@DESKTOP-48IJ5S0>
2024-11-05 20:26:19 +0800

    web

---
+++ WebCart/src/main/java/model/service/CartService.java
@@ -0,0 +1,16 @@
+package model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.dto.OrderDto;
+
+public class CartService {
+	
+	public List<OrderDto> findAllOrdersByUserId(Integer userId) {
+		List<OrderDto> orderDtos = new ArrayList<>();
+		
+		
+		return orderDtos;
+	}
+}
