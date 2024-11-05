054c7931af38cfe4e041a223db792ad709f898aa
FH <FH@DESKTOP-48IJ5S0>
2024-11-05 20:47:14 +0800

    web

--- WebCart/src/main/java/model/service/CartService.java
+++ WebCart/src/main/java/model/service/CartService.java
@@ -3,13 +3,36 @@ package model.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.OrderDao;
+import model.dao.OrderDaoImpl;
 import model.dto.OrderDto;
+import model.entity.Order;
 
 public class CartService {
+	
+	private OrderDao orderDao = new OrderDaoImpl();
 	
 	public List<OrderDto> findAllOrdersByUserId(Integer userId) {
 		List<OrderDto> orderDtos = new ArrayList<>();
 		
+		// 從資料表抓出該使用者的原始訂單資料
+		List<Order> orders = orderDao.findAllOrdersByUserId(userId);
+		for(Order order : orders) {
+			// cart 的 orderStatus 必須是 Pending, 若不是 Pending 則不做事
+			if(!order.getOrderStatus().equals("Pending")) {
+				continue;
+			}
+			// 建立一個 OrderDto 物件將 order 的資料注入進去
+			OrderDto orderDto = new OrderDto();
+			orderDto.setOrderId(order.getOrderId());
+			orderDto.setUserId(order.getUserId());
+			orderDto.setUsername(null);
+			orderDto.setOrderDate(order.getOrderDate());
+			orderDto.setTotalPrice(order.getTotalPrice());
+			orderDto.setOrderStatus(order.getOrderStatus());
+			// 注入到 orderDtos
+			orderDtos.add(orderDto);
+		}
 		
 		return orderDtos;
 	}
