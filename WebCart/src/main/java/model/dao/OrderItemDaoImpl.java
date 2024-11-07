814c74fcfbd89487ba9bb9b3bb258d31f190a959
FH <FH@DESKTOP-48IJ5S0>
2024-11-07 19:36:17 +0800

    web

--- WebCart/src/main/java/model/dao/OrderItemDaoImpl.java
+++ WebCart/src/main/java/model/dao/OrderItemDaoImpl.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +13,31 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
 	public List<OrderItem> findOrderItemsByOrderId(Integer orderId) {
 		List<OrderItem> orderItems = new ArrayList<>();
 		String sql = "select item_id, order_id, product_id, quantity, unit_price from order_items where order_id = ?";
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, orderId);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return orderItems;
 	}
 
 	@Override
 	public List<OrderItem> findOrderItemsByUserId(Integer userId) {
 		List<OrderItem> orderItems = new ArrayList<>();
+		String sql = "SELECT * FROM web.order_items " +
+				     "where order_id in (SELECT order_id FROM orders where user_id = ?)";
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, userId);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		return orderItems;
 	}
