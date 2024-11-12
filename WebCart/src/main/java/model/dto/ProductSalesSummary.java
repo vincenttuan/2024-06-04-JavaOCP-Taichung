d3e03df85f959d0841c84c252c8970811ef59531
FH <FH@DESKTOP-48IJ5S0>
2024-11-12 19:30:45 +0800

    web

---
+++ WebCart/src/main/java/model/dto/ProductSalesSummary.java
@@ -0,0 +1,28 @@
+package model.dto;
+
+// 銷售統計資訊
+public class ProductSalesSummary {
+	private Integer productId;
+	private String productName;
+	private Double total;
+	
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Double getTotal() {
+		return total;
+	}
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	
+}
