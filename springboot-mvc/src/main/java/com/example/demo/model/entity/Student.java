20c7a0d2198255d562047e428031ece8ce5b37e5
FH <FH@DESKTOP-48IJ5S0>
2024-11-19 19:12:24 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/model/entity/Student.java
+++ springboot-mvc/src/main/java/com/example/demo/model/entity/Student.java
@@ -1,8 +1,63 @@
 package com.example.demo.model.entity;
 
+import java.util.Objects;
+
 public class Student {
 	private String name;
 	private Integer age;
 	private Integer score;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, Integer age, Integer score) {
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", score=" + score + "]";
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getScore() {
+		return score;
+	}
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name, score);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(age, other.age) && Objects.equals(name, other.name) && Objects.equals(score, other.score);
+	}
+	
+	
 	
 }
