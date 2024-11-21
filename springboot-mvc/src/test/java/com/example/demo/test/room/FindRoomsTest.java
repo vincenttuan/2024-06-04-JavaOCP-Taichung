55235dd2328e1d2af27ba379b903bde2738fdf48
FH <FH@DESKTOP-48IJ5S0>
2024-11-21 20:55:35 +0800

    web

--- springboot-mvc/src/test/java/com/example/demo/test/room/FindRoomsTest.java
+++ springboot-mvc/src/test/java/com/example/demo/test/room/FindRoomsTest.java
@@ -1,5 +1,25 @@
 package com.example.demo.test.room;
 
-public class FindRoomsTest {
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.entity.Room;
+import com.example.demo.repository.RoomRepositoryJDBC;
+
+@SpringBootTest
+public class FindRoomsTest {
+	// 測試: RoomRepositoryJDBC
+	
+	@Autowired
+	RoomRepositoryJDBC roomRepositoryJDBC;
+	
+	@Test
+	public void test1() {
+		List<Room> rooms = roomRepositoryJDBC.findAllRooms();
+		System.out.println(rooms);
+	}
+	
 }
