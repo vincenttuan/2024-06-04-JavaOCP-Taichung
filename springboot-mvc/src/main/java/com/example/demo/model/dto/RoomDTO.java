2684d4604c604087e043868364e8382cfbbf80b0
FH <FH@DESKTOP-48IJ5S0>
2024-11-21 19:01:24 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/model/dto/RoomDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomDTO {
+	private Integer roomId; // 對應 room.roomId 欄位
+	private String roomName; // 對應 room.roomName 欄位
+	private Integer roomSize; // 對應 room.roomSize 欄位
+}
