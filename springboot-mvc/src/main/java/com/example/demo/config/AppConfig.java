c07c62faf93673d7b8c1ee9755e9613a612e7577
FH <FH@DESKTOP-48IJ5S0>
2024-11-26 19:15:22 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration // Springboot 啟動完成之前會先執行此程式 
+public class AppConfig {
+	
+	// 由 Springboot 來管理此物件
+	// 其他程式也可以透過 @Autowired 來取得該實體
+	@Bean
+	//@Scope("SingleTon") // 預設:單一物件
+	//@Scope("Prototype") // 多物件
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+}
