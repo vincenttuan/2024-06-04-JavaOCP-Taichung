5e438f5a41c19dc389e9b202f8a25f71761641c7
FH <FH@DESKTOP-48IJ5S0>
2024-11-26 19:16:05 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
+++ springboot-mvc/src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service.impl;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class RoomServiceImpl implements RoomService {
 	
 	@Autowired
 	private RoomRepositoryJDBC roomRepositoryJDBC;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public List<RoomDTO> getAllRooms() {
