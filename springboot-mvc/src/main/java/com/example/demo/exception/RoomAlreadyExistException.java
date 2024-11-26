3ddf266603fb819070d154bf8b48899706e64728
FH <FH@DESKTOP-48IJ5S0>
2024-11-26 20:56:54 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/exception/RoomAlreadyExistException.java
+++ springboot-mvc/src/main/java/com/example/demo/exception/RoomAlreadyExistException.java
@@ -1,5 +1,6 @@
 package com.example.demo.exception;
 
+@SuppressWarnings("serial")
 public class RoomAlreadyExistException extends RoomException {
 
 	public RoomAlreadyExistException(String message) {
--- springboot-mvc/src/main/java/com/example/demo/exception/RoomNotFoundException.java
+++ springboot-mvc/src/main/java/com/example/demo/exception/RoomNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.demo.exception;
 
+@SuppressWarnings("serial")
 public class RoomNotFoundException extends RoomException {
 
 	public RoomNotFoundException() {
