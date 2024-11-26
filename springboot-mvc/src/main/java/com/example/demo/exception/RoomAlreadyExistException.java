34a5728dd81714d0c980f6fa7f8929b90c2ae5f7
FH <FH@DESKTOP-48IJ5S0>
2024-11-26 20:22:18 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/exception/RoomAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class RoomAlreadyExistException extends RoomException {
+
+	public RoomAlreadyExistException(String message) {
+		super(message);
+	}
+	
+}
