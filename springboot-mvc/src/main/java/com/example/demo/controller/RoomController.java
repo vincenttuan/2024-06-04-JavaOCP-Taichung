f12bc3c48a496872aa992b5a32fc8684374faded
FH <FH@DESKTOP-48IJ5S0>
2024-11-28 18:59:32 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/RoomController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/RoomController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Method URI                  功能
@@ -14,6 +16,19 @@ import org.springframework.stereotype.Controller;
  * */
 
 @Controller
+@RequestMapping("/room")
 public class RoomController {
-
+	
+	@GetMapping // 查詢所有會議室(多筆)
+	public String getRooms() {
+		return "room"; // 指向 jsp 檔名
+	}
+	
+	@GetMapping("/{roomId}") // 查詢指定會議室(單筆)
+	public String getRoom() {
+		return "room";
+	}
+	
+	
+	
 }
