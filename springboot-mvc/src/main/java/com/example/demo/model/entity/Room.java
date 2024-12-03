e5a668ca69833aba6d7aa4e0ed5a1233fea87e9a
FH <FH@DESKTOP-48IJ5S0>
2024-12-03 19:46:27 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/model/entity/Room.java
+++ springboot-mvc/src/main/java/com/example/demo/model/entity/Room.java
@@ -1,5 +1,9 @@
 package com.example.demo.model.entity;
 
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Room {
+	
+	@NotNull(message = "id 不可以是空值")
 	private Integer roomId; // 對應 room_id 欄位
+	
+	@Size(min = 1, max = 10, message = "名稱必須限定在 {min} ~ {max} 個字間")
 	private String roomName; // 對應 room_name 欄位
+	
+	@Range(min = 1, max = 200, message = "人數範圍限制在 {min} ~ {max} 人")
 	private Integer roomSize; // 對應 room_size 欄位
 	
 }
