fe8781a355c46a0bab19d9e578d4005f5236bce8
FH <FH@DESKTOP-48IJ5S0>
2024-12-05 21:14:45 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/model/response/ApiResponse.java
+++ springboot-mvc/src/main/java/com/example/demo/model/response/ApiResponse.java
@@ -1,7 +1,6 @@
 package com.example.demo.model.response;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
