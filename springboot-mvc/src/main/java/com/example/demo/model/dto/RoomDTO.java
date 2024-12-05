49110a89540aa350aa93e8f595f6508a4e66b619
FH <FH@DESKTOP-48IJ5S0>
2024-12-05 17:58:11 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/model/dto/RoomDTO.java
+++ springboot-mvc/src/main/java/com/example/demo/model/dto/RoomDTO.java
@@ -17,9 +17,11 @@ public class RoomDTO {
 	@NotNull(message = "id 不可以是空值")
 	private Integer id; // 對應 room.roomId 欄位
 	
+	@NotNull(message = "房名不可以是空值")
 	@Size(min = 1, max = 10, message = "名稱必須限定在 {min} ~ {max} 個字間")
 	private String name; // 對應 room.roomName 欄位
 	
+	@NotNull(message = "人數不可以是空值")
 	@Range(min = 1, max = 200, message = "人數範圍限制在 {min} ~ {max} 人")
 	private Integer size; // 對應 room.roomSize 欄位
 }
