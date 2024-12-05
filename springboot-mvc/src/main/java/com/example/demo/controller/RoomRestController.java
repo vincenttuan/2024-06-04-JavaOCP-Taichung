ce25c1f9b58dd02b203bbb9dccebcb92fe5f4b36
FH <FH@DESKTOP-48IJ5S0>
2024-12-05 19:24:17 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/controller/RoomRestController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.dto.RoomDTO;
+import com.example.demo.service.RoomService;
+
+/**
+請求方法 URL 路徑              功能說明      請求參數                                  回應
+-----------------------------------------------------------------------------------------------------
+GET    /rest/rooms         取得所有房間列表 無                                       成功時返回所有房間的列表及成功訊息。
+GET    /rest/room/{roomId} 取得指定房間資料 roomId (路徑參數，房間 ID)                  成功時返回指定房間資料及成功訊息。
+POST   /rest/room          新增房間       請求體包含 roomDto                         成功時返回成功訊息，無回傳資料。
+PUT    /rest/room/{roomId} 更新指定房間資料 roomId (路徑參數，房間 ID)，請求體包含 roomDto 成功時返回成功訊息，無回傳資料。
+DELETE /rest/room/{roomId} 刪除指定房間    roomId (路徑參數，房間 ID)                  成功時返回成功訊息，無回傳資料。
+*/
+@RestController
+@RequestMapping("/rest")
+public class RoomRestController {
+	
+	@Autowired
+	private RoomService roomService;
+	
+	@GetMapping("/rooms")
+	public List<RoomDTO> getRooms() {
+		List<RoomDTO> roomDTOs = roomService.getAllRooms();
+		return roomDTOs;
+	}
+	
+	
+}
