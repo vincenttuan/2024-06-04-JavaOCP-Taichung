d996434ac306fa69876042bfc5b1f1c2ee8e17c3
FH <FH@DESKTOP-48IJ5S0>
2024-12-05 19:47:00 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/RoomRestController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/RoomRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +44,11 @@ public class RoomRestController {
 	}
 	
 	// 新增房間
+	@PostMapping("/room")
+	public Boolean addRoom(@RequestBody RoomDTO roomDTO) {
+		roomService.addRoom(roomDTO);
+		return true;
+	}
 	
 	
 }
