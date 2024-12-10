3a180c4051bc973de9f136abe2cc7cf88b1fd936
FH <FH@DESKTOP-48IJ5S0>
2024-12-10 21:05:59 +0800

    web

---
+++ springboot-client/src/main/resources/static/js/room.js
@@ -0,0 +1,6 @@
+// 獲取 DOM 元素
+const roomList = document.getElementById('roomList');
+const roomIdInput = document.getElementById('roomId');
+const roomNameInput = document.getElementById('roomName');
+const roomSizeInput = document.getElementById('roomSize');
+const addResult = document.getElementById('addResult');
