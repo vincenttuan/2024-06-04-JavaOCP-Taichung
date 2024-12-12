eda453b079ce4f04a35d64915e9521954d02c1d0
FH <FH@DESKTOP-48IJ5S0>
2024-12-12 19:34:51 +0800

    web

--- springboot-client/src/main/resources/static/js/room.js
+++ springboot-client/src/main/resources/static/js/room.js
@@ -30,7 +30,7 @@ const displayRooms = (rooms) => {
 		// 建立 <li> 標籤
 		const item = document.createElement('li');
 		// 在 <li> 標籤內放入資料
-		item.textContent = 'id:' + room.id + ' name:' + room.name + ' size:' + room.size;
+		item.textContent = '房號:' + room.id + ' 房名:' + room.name + ' 人數:' + room.size;
 		
 		// 將 <li> (item) 放到 <ul> (roomList)
 		roomList.appendChild(item);
