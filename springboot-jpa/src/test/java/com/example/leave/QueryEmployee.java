6a95d23ff900c9a3003cf3a548967bdb82b7716f
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 20:29:58 +0800

    web

--- springboot-jpa/src/test/java/com/example/leave/QueryEmployee.java
+++ springboot-jpa/src/test/java/com/example/leave/QueryEmployee.java
@@ -1,14 +1,25 @@
 package com.example.leave;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.leave.model.entity.Employee;
+import com.example.leave.repository.EmployeeRepository;
 
 @SpringBootTest
 public class QueryEmployee {
+	@Autowired
+	EmployeeRepository employeeRepository;
 	
 	@Test
 	public void test() {
-		
+		List<Employee> employees = employeeRepository.findAll();
+		for(Employee emp : employees) {
+			System.out.println(emp);
+		}
 		
 	}
 	
