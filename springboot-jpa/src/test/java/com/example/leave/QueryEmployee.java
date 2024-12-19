3485b86e3aea30b744b6b036ec77c2f6efbddd5e
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 20:26:33 +0800

    web

---
+++ springboot-jpa/src/test/java/com/example/leave/QueryEmployee.java
@@ -0,0 +1,15 @@
+package com.example.leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QueryEmployee {
+	
+	@Test
+	public void test() {
+		
+		
+	}
+	
+}
