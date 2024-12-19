6503674782644b8c279fd23311b9e43f3576eecb
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 20:46:50 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
+++ springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
@@ -2,8 +2,14 @@ package com.example.leave.model.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 @Entity
@@ -11,16 +17,25 @@ import lombok.Data;
 @Data
 public class LeaveRequest {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column
 	private Integer employeeId;
 	
+	@Column
+	@Temporal(TemporalType.DATE)
 	private Date startDate;
 	
+	@Column
+	@Temporal(TemporalType.DATE)
 	private Date endDate;
 	
+	@Column(length = 255)
 	private String reason;
 	
+	@Column(length = 10)
 	private String status; // PENDING、APPROVED、REJECTED
 
 	
