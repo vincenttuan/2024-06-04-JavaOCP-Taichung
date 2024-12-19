d167de7104ffad7d0f68a6a75809543905beac8f
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 20:57:41 +0800

    web

---
+++ springboot-jpa/src/test/java/com/example/leave/AddLeaveRequest.java
@@ -0,0 +1,15 @@
+package com.example.leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AddLeaveRequest {
+	
+	@Test
+	public void test() { 
+		// 員編 = 1 要休 2024-12-24 ~ 2024-12-26
+		
+	}
+	
+}
