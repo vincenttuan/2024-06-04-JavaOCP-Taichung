6f24349e665f4383539672adf0cef9b202c403c0
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 20:39:26 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
+++ springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
@@ -1,5 +1,7 @@
 package com.example.leave.model.entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -8,5 +10,19 @@ import lombok.Data;
 @Table(name = "leave_request") // 預設資料表名 (此行可以省略)
 @Data
 public class LeaveRequest {
+	
+	private Integer id;
+	
+	private Integer employeeId;
+	
+	private Date startDate;
+	
+	private Date endDate;
+	
+	private String reason;
+	
+	private String status; // PENDING、APPROVED、REJECTED
+
+	
 	
 }
