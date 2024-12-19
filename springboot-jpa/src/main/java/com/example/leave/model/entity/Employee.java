dd77fc32ab7f66e24f58a3c143a5977d12e186d3
FH <FH@DESKTOP-48IJ5S0>
2024-12-19 18:45:21 +0800

    web

---
+++ springboot-jpa/src/main/java/com/example/leave/model/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.example.leave.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity // 此類會在資料庫中建立一個 employee 資料表
+@Table(name = "employee") // 將資料表改為指定名稱(預設是類名小寫)
+public class Employee {
+	
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	private Integer annualLeave; // 特休天數
+	
+}
