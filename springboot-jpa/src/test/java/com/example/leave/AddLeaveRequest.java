2da8d4eba07f5fcd9edddcf8e117d8936ddff73e
FH <FH@DESKTOP-48IJ5S0>
2024-12-24 18:42:04 +0800

    web

--- springboot-jpa/src/test/java/com/example/leave/AddLeaveRequest.java
+++ springboot-jpa/src/test/java/com/example/leave/AddLeaveRequest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.leave.model.entity.Employee;
 import com.example.leave.model.entity.LeaveRequest;
+import com.example.leave.repository.EmployeeRepository;
 import com.example.leave.repository.LeaveRequestRepository;
 
 @SpringBootTest
@@ -16,17 +18,23 @@ public class AddLeaveRequest {
 	@Autowired
 	LeaveRequestRepository leaveRequestRepository;
 	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
 	@Test
 	public void test() { 
 		// 員編 = 1 要休 2024-12-24 ~ 2024-12-26
 		try {
+			// 將員工編號 = 1 的資料取出
+			Employee employee = employeeRepository.findById(1).get();
+			
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date startDate = sdf.parse("2024-12-24");
 			Date endDate = sdf.parse("2024-12-26");
 			
 			// 建立假單
 			LeaveRequest leaveRequest = new LeaveRequest();
-			leaveRequest.setEmployeeId(1);
+			leaveRequest.setEmployee(employee);
 			leaveRequest.setType("特休");
 			leaveRequest.setStartDate(startDate);
 			leaveRequest.setEndDate(endDate);
