72f02ddad5a27c3de601c69fd202daac68160311
FH <FH@DESKTOP-48IJ5S0>
2024-12-24 19:01:09 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
+++ springboot-jpa/src/main/java/com/example/leave/model/entity/LeaveRequest.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
