b412ca40b934468e47786f14b1089b4facab7168
FH <FH@DESKTOP-48IJ5S0>
2024-12-26 19:22:38 +0800

    web

--- springboot-jpa/src/test/java/com/example/leave/test/repository/AddProjectToEmployee.java
+++ springboot-jpa/src/test/java/com/example/leave/test/repository/AddProjectToEmployee.java
@@ -20,18 +20,32 @@ public class AddProjectToEmployee {
 	
 	@Test
 	public void test() {
-		// 取得專案(請先自行查詢資料表的紀錄)
-		Project web = projectRepository.findById(1).get();
-		Project ppt = projectRepository.findById(2).get();
-		Project app = projectRepository.findById(3).get();
-		
-		// 取得員工(請先自行查詢資料表的紀錄) 
-		Employee john = employeeRepository.findById(1).get();
-		Employee mary = employeeRepository.findById(3).get();
-		Employee david = employeeRepository.findById(4).get();
-		
-		
-		
+		try {
+			// 取得專案(請先自行查詢資料表的紀錄)
+			Project web = projectRepository.findById(1).get();
+			Project ppt = projectRepository.findById(2).get();
+			Project app = projectRepository.findById(3).get();
+			
+			// 取得員工(請先自行查詢資料表的紀錄) 
+			Employee john = employeeRepository.findById(1).get();
+			Employee mary = employeeRepository.findById(3).get();
+			Employee david = employeeRepository.findById(4).get();
+			
+			// 員工自行認領專案
+			john.getProjects().add(web);
+			john.getProjects().add(ppt);
+			john.getProjects().add(app);
+			employeeRepository.save(john);
+			
+			mary.getProjects().add(web);
+			mary.getProjects().add(app);
+			employeeRepository.save(mary);
+			
+			david.getProjects().add(ppt);
+			employeeRepository.save(david);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
