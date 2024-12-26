3e1945b6c1afb8b95025eda95b4c5dfd4c81f41b
FH <FH@DESKTOP-48IJ5S0>
2024-12-26 20:42:02 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
+++ springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
@@ -21,11 +21,13 @@ public class EmployeeService {
 	
 	// 查詢所有員工資料
 	public List<EmployeeDTO> findAllEmployeeDTOs() {
+		// 先從資料庫中抓出 List<Employee>
 		List<Employee> employees = employeeRepository.findAll();
 		// 利用 ModelMapper 將 Employee 轉 EmployeeDTO
-		List<EmployeeDTO> employeeDTOs = employees.stream()
-												  .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
-												  .toList();
+		List<EmployeeDTO> employeeDTOs = employees // List<Employee>
+											.stream() // Employee, Employee, Employee ...
+											.map(employee -> modelMapper.map(employee, EmployeeDTO.class)) // EmployeeDTO, EmployeeDTO, EmployeeDTO ...
+											.toList(); // List<EmployeeDTO>
 		return employeeDTOs;
 	
 	}
