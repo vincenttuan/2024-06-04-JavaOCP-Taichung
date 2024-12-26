bd8640bfdd8703c170d610054cc24baf349906b8
FH <FH@DESKTOP-48IJ5S0>
2024-12-26 20:34:40 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
+++ springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
@@ -1,8 +1,25 @@
 package com.example.leave.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.leave.model.dto.EmployeeDTO;
+import com.example.leave.model.entity.Employee;
+import com.example.leave.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
-
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
+	
+	// 查詢所有員工資料
+	public List<EmployeeDTO> findAllEmployeeDTOs() {
+		List<Employee> employees = 
+	}
+	
 }
