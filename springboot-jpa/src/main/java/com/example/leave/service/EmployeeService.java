aa28c516537a9a24dcad49c67d9867446017d1f7
FH <FH@DESKTOP-48IJ5S0>
2024-12-26 20:39:13 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
+++ springboot-jpa/src/main/java/com/example/leave/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.example.leave.service;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +16,18 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	// 查詢所有員工資料
 	public List<EmployeeDTO> findAllEmployeeDTOs() {
-		List<Employee> employees = 
+		List<Employee> employees = employeeRepository.findAll();
+		// 利用 ModelMapper 將 Employee 轉 EmployeeDTO
+		List<EmployeeDTO> employeeDTOs = employees.stream()
+												  .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
+												  .toList();
+		return employeeDTOs;
+	
 	}
 	
 }
