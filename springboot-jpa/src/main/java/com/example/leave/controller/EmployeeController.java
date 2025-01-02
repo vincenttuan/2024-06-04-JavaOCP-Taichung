9412f1ec5ed9a5076ba339f971109e16f8d0d417
FH <FH@DESKTOP-48IJ5S0>
2025-01-02 18:59:00 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
+++ springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.example.leave.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.leave.model.dto.EmployeeDTO;
 import com.example.leave.service.EmployeeService;
@@ -19,8 +24,23 @@ public class EmployeeController {
 	
 	// 員工註冊 register / 新增員工
 	@GetMapping("/register")
-	public String register(EmployeeDTO employeeDTO) {
-		return "employee_register";
+	public String register() {
+		return "employee_register"; // 員工註冊 jsp
+	}
+	
+	// 員工註冊 (接收 employee_register.jsp 員工註冊 jsp 表單傳來的資訊)
+	@PostMapping("/register")
+	public String addEmployee(@RequestParam(name = "username") String username, 
+			@RequestParam(name = "password") String password, 
+			@RequestParam(name = "salaryAmount") Integer salaryAmount, 
+			@RequestParam(name = "arrivalDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date arrivalDate, 
+			@RequestParam(name = "annualLeave") Integer annualLeave,
+			Model model) {
+		
+		employeeService.addEmployee(username, password, salaryAmount, arrivalDate, annualLeave);
+		
+		model.addAttribute("message", "員工註冊成功");
+		return "result";
 	}
 	
 	
