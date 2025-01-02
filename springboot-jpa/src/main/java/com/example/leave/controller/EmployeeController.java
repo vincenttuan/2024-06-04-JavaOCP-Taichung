6d3ef20d13ddf057bc820edf787a021e07646853
FH <FH@DESKTOP-48IJ5S0>
2025-01-02 19:14:30 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
+++ springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
@@ -7,12 +7,10 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.leave.model.dto.EmployeeDTO;
 import com.example.leave.service.EmployeeService;
 
 @Controller
@@ -30,7 +28,7 @@ public class EmployeeController {
 	
 	// 員工註冊 (接收 employee_register.jsp 員工註冊 jsp 表單傳來的資訊)
 	@PostMapping("/register")
-	public String addEmployee(@RequestParam(name = "username", defaultValue = "John") String username, 
+	public String addEmployee(@RequestParam(name = "username") String username, 
 			@RequestParam(name = "password") String password, 
 			@RequestParam(name = "salaryAmount") Integer salaryAmount, 
 			@RequestParam(name = "arrivalDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date arrivalDate, 
