3b0276a3efed0785a629b34232b25677e7f733d5
FH <FH@DESKTOP-48IJ5S0>
2025-01-07 19:41:07 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/example/leave/SpringbootJpaApplication.java
@@ -2,8 +2,10 @@ package com.example.leave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 啟用傳統的 Java Web @註解配置
 public class SpringbootJpaApplication {
 
 	public static void main(String[] args) {
--- springboot-jpa/src/main/java/com/example/leave/filter/CheckLoginFilter.java
+++ springboot-jpa/src/main/java/com/example/leave/filter/CheckLoginFilter.java
@@ -2,11 +2,8 @@ package com.example.leave.filter;
 
 import java.io.IOException;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
