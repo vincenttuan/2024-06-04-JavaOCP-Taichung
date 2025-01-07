bfc25c5aa9e6205c88f40201fe955556936bec13
FH <FH@DESKTOP-48IJ5S0>
2025-01-07 21:00:13 +0800

    web

---
+++ springboot-jpa/src/main/java/com/example/leave/controller/ProjectController.java
@@ -0,0 +1,35 @@
+package com.example.leave.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.leave.model.dto.ProjectDTO;
+import com.example.leave.service.ProjectService;
+
+@Controller
+public class ProjectController {
+	
+	@Autowired
+	private ProjectService projectService;
+	
+	@GetMapping("/project")
+	public String findAllProjects(ProjectDTO projectDTO, Model model) {
+		List<ProjectDTO> projectDTOs = projectService.findAllProjectDTOs();
+		model.addAttribute("projectDTOs", projectDTOs);
+		return "project";
+	}
+	
+	@PostMapping("/project")
+	public String addProject(ProjectDTO projectDTO) {
+		projectService.addProject(projectDTO);
+		return "redirect:/project";
+	}
+	
+	
+}
