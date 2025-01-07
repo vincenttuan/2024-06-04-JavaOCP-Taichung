c649b411ffff4829a62fcfe4a7917da844e69045
FH <FH@DESKTOP-48IJ5S0>
2025-01-07 19:51:55 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/filter/CheckLoginFilter.java
+++ springboot-jpa/src/main/java/com/example/leave/filter/CheckLoginFilter.java
@@ -1,6 +1,7 @@
 package com.example.leave.filter;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,12 +11,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(urlPatterns = {"/employee", "/project/*", "/leave_request", "/"})
+@WebFilter(urlPatterns = {"/*"})
 public class CheckLoginFilter extends HttpFilter{
-
+	
+	// 白名單
+	private static List<String> whitelists = List.of("/login", "/employee/register");
+	
 	@Override
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
+		String servletPath = request.getServletPath(); // 取得當前 URL servletPath
+		if(whitelists.contains(servletPath)) { // servletPath 是否包含在 whitelists 中
+			// 此為白名單路徑, 放行
+			chain.doFilter(request, response);
+			return;
+		}
 		
 		// 檢查 session 中是否有登入資訊
 		HttpSession session = request.getSession();
