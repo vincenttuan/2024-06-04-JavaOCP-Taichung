1cc021768fb0cbdbaf5605034ed5968c5b344259
FH <FH@DESKTOP-48IJ5S0>
2025-01-14 20:01:31 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/model/dto/LeaveRequestDTO.java
+++ springboot-jpa/src/main/java/com/example/leave/model/dto/LeaveRequestDTO.java
@@ -14,10 +14,13 @@ import lombok.Setter;
 public class LeaveRequestDTO {
 	private Integer id;
 	private String type; // 特休, 病假, 事假
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd") // 指定只有日期
 	private Date startDate;
+	
 	@DateTimeFormat(pattern = "yyyy-MM-dd") // 指定只有日期
 	private Date endDate;
+	
 	private String reason;
 	private String status; // "PENDING", "APPROVED", "REJECTED"
 	
