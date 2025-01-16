ea86baae8cc0871b32974af3fd1fca6113e0983f
FH <FH@DESKTOP-48IJ5S0>
2025-01-16 20:46:05 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/repository/LeaveRequestRepository.java
+++ springboot-jpa/src/main/java/com/example/leave/repository/LeaveRequestRepository.java
@@ -1,6 +1,9 @@
 package com.example.leave.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.leave.model.entity.LeaveRequest;
@@ -8,5 +11,13 @@ import com.example.leave.model.entity.LeaveRequest;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Integer> {
-
+	// 查詢某員工的請假資料
+	// nativeQuery=true 表示是使用標準 sql 語言
+	@Query(value = "select * from leave_request where employee_id = :employeeId", nativeQuery=true)
+	List<LeaveRequest> query1(Integer employeeId);
+	
+	
+	
+	// 查詢在指定日期間的請假資料
+	
 }
